8df812dfdee878e16ce47694b58e961d73aeb84c
Yonezcy <364251097@qq.com>
2017-11-14 18:29:14 +0800

    增加11月14日算法，求str2

---
+++ Implement strStr().java
@@ -0,0 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals("")) return 0;
+        if (haystack.equals("") && !needle.equals("")) return -1;
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        int i, j;
+        for (i=0; i<len1; i++) {
+            if ((i+len2) > len1) break;
+            if (haystack.substring(i, i+len2).equals(needle)) return i;
+        }
+        return -1;
+    }
+}

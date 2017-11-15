5dce2875877c307deac62add013c9268afdd102e
Yonezcy <364251097@qq.com>
2017-11-15 17:25:56 +0800

    修改代码规范

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -8,21 +8,19 @@
  */
 class Solution {
 public:
-    struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
     {
+        if (!l1) return l2;
+        if (!l2) return l1;
         int x = 0;
-
-        struct ListNode* node = (struct ListNode*) malloc (sizeof(struct ListNode));
-
-        struct ListNode* head = node;
-
-        while(l1!=NULL || l2!=NULL || x!=0)    
+        ListNode* node = (ListNode*) malloc (sizeof(ListNode));
+        ListNode* head = node;
+        while(l1 || l2 || x )    
         {
             node->val = 0;
             if (l1 != NULL) node->val += l1->val;
             if (l2 != NULL) node->val += l2->val;
             if (x != 0) node->val += x;
-
             if (node->val > 9)
             {
                 node->val -= 10;  
@@ -33,7 +31,6 @@ public:
             if (l1 != NULL)
                 l1 = l1->next; 
 
-
             if (l2 != NULL)
                 l2 = l2->next; 
 
@@ -42,10 +39,8 @@ public:
                 node->next = (struct ListNode*) malloc (sizeof(struct ListNode));
                 node = node->next;             
             }   
-
         }  
         node->next = NULL;  
-
         return head;
     }
 };

12032305ebce3b2659c5f4c4e44d685ee485d84f
Yonezcy <364251097@qq.com>
2017-11-15 19:31:29 +0800

    增加不需要消耗额外空间的方法（超时）

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -40,7 +40,62 @@ public:
                 node = node->next;             
             }   
         }  
-        node->next = NULL;  
+        node->next = NULL;   
+        return head;
+    }
+};
+
+// Time exceed function
+/*
+Not using extra space
+*/
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        int len1 = 0, len2 = 0;
+        ListNode* newL1;
+        ListNode* newL2;
+        newL1 = l1; newL2 = l2;
+        
+        while (l1) {
+            len1++;
+            l1 = l1->next;
+        }
+        while (l2) {
+            len2++;
+            l2 = l2->next;
+        }
+        
+        ListNode* head;
+        int x = 0;
+        if (len1 > len2)
+            head = newL1;
+        else head = newL2;
+        
+        while (newL1 || newL2 || x) {
+            if (len1 > len2 && newL2) {
+                newL1->val = newL1->val + newL2->val + x;
+                if (newL1->val >= 10) {
+                    newL1->val -= 10;
+                    x = 1;
+                }
+                else x = 0;
+            }
+            if (len2 >= len1 && newL1) {
+                newL2->val = newL2->val + newL1->val + x;
+                if (newL2->val >= 10) {
+                    newL2->val -= 10;
+                    x = 1;
+                }
+                else x = 0;
+            }
+            if (newL1) newL1 = newL1->next;
+            if (newL2) newL2 = newL2->next;
+        }
+        
         return head;
     }
 };

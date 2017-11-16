edc30b3cf02d35dadea197522c585d432553eaaa
Yonezcy <364251097@qq.com>
2017-11-16 19:48:34 +0800

    增加11月16日算法，将链表中小于target的结点排在前面

---
+++ Partition List.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ // Accepted solution
+ class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode left(0), right(0);
+        ListNode *l = &left, *r = &right;
+
+        while(head){
+            ListNode* & ref = head->val < x ? l : r;
+            ref->next = head;
+            ref = ref->next;
+            
+            head = head->next;
+        }
+        l->next = right.next;
+        r->next = NULL;
+        return left.next;
+    }
+};
+
+ // Two pointer solution(not accepted)
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (head == NULL || head->next == NULL) return head;
+        
+        ListNode* fakehead = (ListNode*)malloc(sizeof(ListNode));
+        fakehead->next = head;
+        ListNode* ptr1 = fakehead;
+        ListNode* ptr2 = head;
+        ListNode* nextstart = (ListNode*)malloc(sizeof(ListNode));
+        
+        while (ptr2->next) {
+            if (ptr2->next->val < x) {
+                nextstart = ptr2->next;
+                ptr2->next = ptr2->next->next;
+                nextstart->next = ptr1->next;
+                ptr1->next = nextstart;
+                ptr1 = ptr1->next;
+            }
+            else ptr2 = ptr2->next;
+        }
+        return fakehead->next;
+    }
+};

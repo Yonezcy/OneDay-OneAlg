7357edeb999c7841756325b3d6693041e3500434
Yonezcy <364251097@qq.com>
2017-11-21 23:19:07 +0800

    修改格式

--- Reverse Linked List II.cpp
+++ Reverse Linked List II.cpp
@@ -11,9 +11,9 @@ public:
     ListNode *reverseBetween(ListNode *head, int m, int n) {
         if (m == n) return head;
         n -= m; 
-        ListNode prehead(0);
-        prehead.next = head;
-        ListNode* pre = &prehead;
+        ListNode* prehead = (ListNode*)malloc(sizeof(ListNode));
+        prehead->next = head;
+        ListNode* pre = prehead;
         while (--m) pre = pre->next;        
         ListNode* pstart = pre->next;
         while (n--)
@@ -23,6 +23,6 @@ public:
             p->next = pre->next;
             pre->next = p;
         }
-        return prehead.next;
+        return prehead->next;
     }
 };

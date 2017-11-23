6d454cbfb5a7492e9e2b5e93f2ba7736784e5eb8
Yonezcy <364251097@qq.com>
2017-11-23 21:18:33 +0800

    修改错误，增加注释

--- Longest Common Prefix.java
+++ Longest Common Prefix.java
@@ -32,6 +32,7 @@ class Solution {
         if (strs.length == 0) return "";
         String prefix = strs[0];
         for (int i=1; i<strs.length; i++)
+            // function indexOf() return the first index of prefix in strs[i]
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length()-1);
                 if (prefix.isEmpty()) return "";
@@ -43,7 +44,7 @@ class Solution {
 class Solution {
     // vertical scanning
     public String longestCommonPrefix(String[] strs) {
-        if (strs == nul || strs.length == 0) return "";
+        if (strs == null || strs.length == 0) return "";
         for (int i=0; i<strs[0].length; i++) {
             char c = strs[0].charAt(i);
             for (int j=i; j<strs.length; j++) {

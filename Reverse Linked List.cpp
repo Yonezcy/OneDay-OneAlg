7050263ac53128e33fee280cb5a050f4dd3fbabe
Yonezcy <364251097@qq.com>
2017-11-24 14:08:25 +0800

    增加11月24日算法，求链表逆序(递归解法)

--- Reverse Linked List.cpp
+++ Reverse Linked List.cpp
@@ -22,3 +22,14 @@ public:
         return pre;
     }
 };
+// recursive solution
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return p;
+    }
+};

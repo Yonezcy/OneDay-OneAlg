5520e48d75fd233b6404c2353fb026158b9bed16
Yonezcy <364251097@qq.com>
2017-11-24 21:36:42 +0800

    增加注释

--- Permutation_algorithm.py
+++ Permutation_algorithm.py
@@ -20,6 +20,7 @@ def permutation(array, begin, end):
         print '\n'
         return
 
+    # one for cycle is a horizontal layer, like (1,2,3,4) (2,1,3,4) (3,2,1,4) (4,2,3,1)
     for i in range(begin, end):
         array[i], array[begin] = swap(array[i], array[begin])
         permutation(array, begin+1, end)

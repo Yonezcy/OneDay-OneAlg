2e828e1ba67c7d309dbd068ef1ef911ca722d501
Yonezcy <364251097@qq.com>
2017-12-19 16:55:11 +0800

    增加12月19日算法，判断两个二叉树是否相等

---
+++ Same Tree.java
@@ -0,0 +1,20 @@
+// leetcode 100th problem
+// judge whether the two binary trees are equal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+}

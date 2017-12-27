69a5862b164f95ce5edd6dc5b96f33158e8550f5
Yonezcy <364251097@qq.com>
2017-12-27 09:48:30 +0800

    增加12月27日算法，把一个数组划分为两部分，使两部分的和相等(0-1背包问题，腾讯面试题)

--- Partition Equal Subset Sum.java
+++ Partition Equal Subset Sum.java
@@ -2,6 +2,9 @@
 // 把一个数组划分为两部分，使两部分的和相等
 // 0-1背包问题，dp solution
 
+// dp[i][j] means whether the specific sum j can be gotten from the first i numbers. 
+// If we can pick such a series of numbers from 0-i whose sum is j, dp[i][j] is true, otherwise it is false.
+
 // For each number, if we don't pick it, dp[i][j] = dp[i-1][j], which means if the first i-1 elements 
 // has made it to j, dp[i][j] would also make it to j (we can just ignore nums[i]). 
 // If we pick nums[i]. dp[i][j] = dp[i-1][j-nums[i]], which represents that j is composed of the 

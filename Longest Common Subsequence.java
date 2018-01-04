6d532385a80629088bcf96eecbcbd9f2e41cf44c
Yonezcy <364251097@qq.com>
2018-01-04 10:28:09 +0800

    增加1月4日算法，求两个数组的最长公共子序列

---
+++ Longest Common Subsequence.java
@@ -0,0 +1,24 @@
+// 求两个数组的最长公共子序列的长度
+
+class Solution {
+    public int findLength(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m == 0 || n == 0) return 0;
+        //dp[i][j] is the length of longest common subsequence ending with nums[i] and nums[j]
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 0;i <= m;i++){
+            for(int j = 0;j <= n;j++){
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else{
+                    if(A[i-1] == B[j-1])
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    else 
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}

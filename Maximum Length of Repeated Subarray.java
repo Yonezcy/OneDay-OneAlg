2d53b90791de954a02b4e3c7a63ab36e94409ac7
Yonezcy <364251097@qq.com>
2018-01-04 10:29:36 +0800

    增加1月4日算法，求两个数组的最长公共子串

--- Maximum Length of Repeated Subarray.java
+++ Maximum Length of Repeated Subarray.java
@@ -9,13 +9,13 @@ class Solution {
         int max = 0;
         //dp[i][j] is the length of longest common subarray ending with nums[i] and nums[j]
         int[][] dp = new int[m + 1][n + 1];
-        for(int i = 0;i <= m;i++){
-            for(int j = 0;j <= n;j++){
-                if(i == 0 || j == 0)
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
                     dp[i][j] = 0;
-                else{
-                    if(A[i - 1] == B[j - 1]){
-                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                else {
+                    if (A[i-1] == B[j-1]) {
+                        dp[i][j] = 1 + dp[i-1][j-1];
                         max = Math.max(max,dp[i][j]);
                     }
                 }

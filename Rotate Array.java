c9a775b464abdb4f1b933080670838dfc67d2f8e
Yonezcy <364251097@qq.com>
2018-01-05 10:12:28 +0800

    增加1月5日算法，将一位数组后k个数移到前面

---
+++ Rotate Array.java
@@ -0,0 +1,21 @@
+// leetcode 189 problem
+// 将一位数组中的后k个数移到前面
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

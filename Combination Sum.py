9b877dc83df7df19c1598f73da45de737f8888e9
Yonezcy <364251097@qq.com>
2018-01-06 09:43:22 +0800

    增加1月6日算法，求数组中和为target的序列

---
+++ Combination Sum.py
@@ -0,0 +1,18 @@
+# leetcode 39 problem
+# 求数组中和为target的序列
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res)
+        return res
+
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return  # backtracking
+        if target == 0:
+            res.append(path)
+            return 
+        for i in xrange(index, len(nums)):
+            self.dfs(nums, target-nums[i], i, path+[nums[i]], res)

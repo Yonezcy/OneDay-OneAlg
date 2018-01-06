70833bbafd6bfb03c7a67322116cde09090d3853
Yonezcy <364251097@qq.com>
2018-01-06 10:35:43 +0800

    增加1月6日算法，求数组中和为target的序列（单个序列中不允许重复取数）

---
+++ Combination Sum II.py
@@ -0,0 +1,23 @@
+# leetcode 40 problem 
+# 求数组中和为target的序列（单个序列中不允许重复取数）
+
+# dfs实现
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res)
+        return res
+
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return  # backtracking
+        if target == 0:
+            if path not in res:
+                res.append(path)
+            return 
+        if index > len(nums):
+            return
+        for i in xrange(index, len(nums)):
+            self.dfs(nums, target-nums[i], i+1, path+[nums[i]], res)
+        

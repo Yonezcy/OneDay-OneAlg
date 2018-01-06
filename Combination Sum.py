8541c595c1ac324cc367f2d01879d64fb09790c8
Yonezcy <364251097@qq.com>
2018-01-06 10:36:58 +0800

    增加1月6日算法，求数组中和为target的序列（单个序列中允许重复取数）

--- Combination Sum.py
+++ Combination Sum.py
@@ -1,6 +1,7 @@
 # leetcode 39 problem
-# 求数组中和为target的序列
+# 求数组中和为target的序列（单个序列中允许重复取数）
 
+# dfs solution
 class Solution(object):
     def combinationSum(self, candidates, target):
         res = []

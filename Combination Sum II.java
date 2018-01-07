480b3957b7b64bb5cdbf20aaac51a751aacfdd77
Yonezcy <364251097@qq.com>
2018-01-07 10:29:30 +0800

    增加1月7日算法，求数组中和为target的序列，允许重复取数（经典回溯，类比permutation、subset记忆）

---
+++ Combination Sum II.java
@@ -0,0 +1,26 @@
+// leetcode 40 problem
+// 求数组中和为target的序列（允许重复取数）
+
+// 经典回溯，类比permutation、subset记忆
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else {
+            for(int i = start; i < nums.length; i++){
+                if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i + 1);
+                tempList.remove(tempList.size() - 1); 
+            }
+        }
+    } 
+}

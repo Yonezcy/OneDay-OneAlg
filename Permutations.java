81955b12fee71f1fe899c82cfdb32f335126dbb4
Yonezcy <364251097@qq.com>
2018-01-07 10:19:54 +0800

    增加1月7日算法，求全排列（java实现，经典回溯，类比subset、combinationsum记忆）

---
+++ Permutations.java
@@ -0,0 +1,25 @@
+// leetcode 46 problem
+// 求全排列
+
+// 回溯法（java实现，类比subset、combination sum记忆）
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+       List<List<Integer>> list = new ArrayList<>();
+       // Arrays.sort(nums); // not necessary
+       backtrack(list, new ArrayList<>(), nums);
+       return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+       if(tempList.size() == nums.length){
+          list.add(new ArrayList<>(tempList));
+       } else{
+          for(int i = 0; i < nums.length; i++){ 
+             if(tempList.contains(nums[i])) continue; // element already exists, skip
+             tempList.add(nums[i]);
+             backtrack(list, tempList, nums);
+             tempList.remove(tempList.size() - 1);
+          }
+       }
+    } 
+}

6cd9241575aa94b0e7bb34ff6cf8ed9035e4ec05
Yonezcy <364251097@qq.com>
2018-01-07 10:14:18 +0800

    增加1月7日算法，求一维数组的所有子集（给的数组中有重复）

---
+++ Subsets II.java
@@ -0,0 +1,22 @@
+// leetcode 90 problem
+// 求一维数组的所有子集（给的数组中有重复）
+
+// 经典回溯解法，背下来
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i+1);
+            tempList.remove(tempList.size()-1);
+        }
+    } 
+}

3aef74a714acefdc9d0cdbcfeef5eaf628251792
Yonezcy <364251097@qq.com>
2018-01-12 10:02:29 +0800

    增加1月10日算法，删除二叉树的一个结点

---
+++ Delete Node in a BST.java
@@ -0,0 +1,40 @@
+// leetcode 450 problem
+// 删除二叉树的一个结点
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+
+        if (key < root.val)
+            root.left = deleteNode(root.left, key);
+        else if (key > root.val)
+            root.right = deleteNode(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            TreeNode minNode = findMin(root.right);
+            // 最后将右子树的值赋给根
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+}

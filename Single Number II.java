2833241b1ab1f7a24d2aecdeb2ada7b2744fc9e5
Yonezcy <364251097@qq.com>
2018-01-14 10:15:33 +0800

    增加1月14日算法，数组中每个数都出现3次，只有1个数出现1次，找出它

---
+++ Single Number II.java
@@ -0,0 +1,17 @@
+// leetcode 137 problem
+// 数组中每个数都出现3次，只有1个数出现1次，找出它
+
+// Let ‘one’ represents the first bit, ‘two’ represents the second bit. Then we need to set rules for ‘one’ 
+// and ‘two’ so that they act as we hopes. The complete loop is 00->10->01->00(0->1->2->3/0).
+class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int one = 0, two = 0;
+        for (int i = 0; i < nums.length; i++) {
+            one = (one ^ nums[i]) & (~two);
+            two = (two ^ nums[i]) & (~one);
+        }
+        return one;
+    }
+}

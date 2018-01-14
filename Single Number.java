994a70cc2d1b52352e652555f84f0d9c7d7a73b6
Yonezcy <364251097@qq.com>
2018-01-14 09:39:15 +0800

    增加1月14日算法，数组中所有数字出现两次，只有一个出现一次，求出它

--- Single Number.java
+++ Single Number.java
@@ -1,11 +1,14 @@
-// Single number problem
+// leetcode 136 problem
 // 数组中所有数字出现两次，只有一个出现一次，求出它
+
 class Solution {
-public int singleNumber(int A[], int n) {
-    int x = 0;
-    for (int i = 0; i < n; i++)
-    	// 异或可消除两个相同位
-    	x ^= A[i];
-    return x;
-  }
-};
+    public int singleNumber(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i]; 
+        }
+        return res;
+    }
+}

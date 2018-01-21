c8e8dec34f6da4b945970beb89a65723ec59cd23
Yonezcy <364251097@qq.com>
2018-01-21 11:09:50 +0800

    增加1月21日算法，将一维数组中的非零元素按原来的顺序移到0元素的前面

---
+++ Move Zeroes.java
@@ -0,0 +1,15 @@
+// leetcode 283 problem
+// 将一维数组中的非零元素按原来的顺序移到0元素的前面
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == null || nums.length == 0) return;
+        
+        int insertPosition = 0;
+        for (int num : nums) 
+            if (num != 0) nums[insertPosition++] = num;
+        
+        while (insertPosition < nums.length) 
+            nums[insertPosition++] = 0;
+    }
+}

c29d9e762b5eea1a35e04cc0bb436f17828c081f
Yonezcy <364251097@qq.com>
2018-01-22 11:05:48 +0800

    增加1月22日算法，求二叉树的中序遍历（根左右）

---
+++ Binary Tree Inorder Traversal.java
@@ -0,0 +1,31 @@
+// leetcode 94 problem
+// 求二叉树的中序遍历（根左右）
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
+}

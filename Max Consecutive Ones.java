d9554a32e87dc9f474c31c0387be69277a39f346
Yonezcy <364251097@qq.com>
2018-01-23 12:43:23 +0800

    增加1月23日算法，找出一维数组中最大连续的1的个数

---
+++ Max Consecutive Ones.java
@@ -0,0 +1,18 @@
+// leetcode 485 problem
+// 找出一维数组中最大连续的1的个数
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int count = 0, max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                max = Math.max(max, count);
+            }
+            else count = 0;
+        }
+        return max;
+    }
+}

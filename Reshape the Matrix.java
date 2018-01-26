a1b164d27307d64494aff7fd0d31c2e38d20b7f4
Yonezcy <364251097@qq.com>
2018-01-26 12:16:39 +0800

    实现matlab里的reshape函数

---
+++ Reshape the Matrix.java
@@ -0,0 +1,62 @@
+// leetcode 566 problem
+// 实现matlab里的reshape函数
+
+// solution 1 using queue
+class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int[][] res = new int[r][c];
+        if (nums.length == 0 || r * c != nums.length * nums[0].length)
+            return nums;
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                queue.add(nums[i][j]);
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                res[i][j] = queue.remove();
+            }
+        }
+        return res;
+    }
+}
+
+// solution 2 without using extra space
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int[][] res = new int[r][c];
+        if (nums.length == 0 || r * c != nums.length * nums[0].length)
+            return nums;
+        int rows = 0, cols = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                res[rows][cols] = nums[i][j];
+                cols++;
+                if (cols == c) {
+                    rows++;
+                    cols = 0;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+// solution 3 using division and modulus
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int[][] res = new int[r][c];
+        if (nums.length == 0 || r * c != nums.length * nums[0].length)
+            return nums;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                res[count/c][count%c] = nums[i][j];
+                count++;
+            }
+        }
+        return res;
+    }
+}

65643a39711c4cd1afa091e174d07e27c28e26f1
Yonezcy <364251097@qq.com>
2018-01-28 22:50:03 +0800

    增加1月28日算法，判断一个数是否为任意两个数的平方和

---
+++ Sum of Square Numbers.java
@@ -0,0 +1,13 @@
+// leetcode 633 problem
+// 判断一个数是否为任意两个数的平方和
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b)
+                return true;
+        }
+        return false;
+    }
+}

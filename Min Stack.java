253ad36c86861c7e1d0adba5d3460d9d2bf8999a
Yonezcy <364251097@qq.com>
2018-01-29 11:16:49 +0800

    增加1月29日算法，构建一个栈，实现push、pop、top、getmin操作

---
+++ Min Stack.java
@@ -0,0 +1,31 @@
+// leetcode 155 problem
+// 构建一个栈，实现push、pop、top、getmin操作
+// the reason why pop and push twice is we need to maintain the old minumum value 
+
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min = stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}

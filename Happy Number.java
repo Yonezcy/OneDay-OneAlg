fd9431a965e9406c8ce1cf3a863bc0f20e6169fd
Yonezcy <364251097@qq.com>
2018-01-30 19:35:55 +0800

    增加1月30日算法，判断一个数是否是happy number（各个位上的数的平方和循环相加后等于1）

---
+++ Happy Number.java
@@ -0,0 +1,26 @@
+// leetcode 202 problem
+// 判断一个数是否是happy number（各个位上的数的平方和循环相加后等于1）
+
+class Solution {
+    public int digitSquareSum(int n) {
+        int sum = 0, tmp;
+        while (n != 0) {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        int slow, fast;
+        slow = fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(fast);
+            fast = digitSquareSum(fast);
+        } while(slow != fast);
+        if (slow == 1) return true;
+        else return false;
+    }
+}

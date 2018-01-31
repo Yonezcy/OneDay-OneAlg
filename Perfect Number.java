3580c2689c30a2cc8ac432371e2391b196b4fa01
Yonezcy <364251097@qq.com>
2018-01-31 15:45:14 +0800

    增加1月31日算法，判断一个数是否是happy number（各个位上的数的平方和循环相加后等于1）

---
+++ Perfect Number.java
@@ -0,0 +1,22 @@
+// leetcode 507 problem
+// 判断一个数是否是perfect number（除了本身的约数之和为它本身）
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 0) return false;
+        int sum = 0;
+        // 只用判断一半，减少时间复杂度
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                // 防止6*6 = 36，6被重复加的情况
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        // 去掉num本身
+        return sum - num == num;
+    }
+}

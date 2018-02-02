776c5d4eed9baf0d355d348cf5bbd8adf425348f
Yonezcy <364251097@qq.com>
2018-02-02 11:26:27 +0800

    增加2月2日算法，在一维数组中寻找顺序缺失的数（一些数出现两次，另一些一次）

---
+++ Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,22 @@
+// leetcode 448 problem
+// 在一维数组中寻找顺序缺失的数（一些数出现两次，另一些一次）
+
+class Solution {
+     public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ret = new ArrayList<Integer>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if(nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                ret.add(i+1);
+            }
+        }
+        return ret;
+    }
+}

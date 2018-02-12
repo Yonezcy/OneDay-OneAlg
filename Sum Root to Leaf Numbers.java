ddb01145bf076b1db57a23f070292758dda8d297
Yonezcy <364251097@qq.com>
2018-02-12 10:21:16 +0800

    增加2月12日算法，找出二叉树代表的数字总和

---
+++ Sum Root to Leaf Numbers.java
@@ -0,0 +1,23 @@
+// leetcode 129 problem
+// 找出二叉树代表的数字总和
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return sum(root, 0);
+    }
+
+    public int sum(TreeNode n, int s){
+        if (n == null) return 0;
+        if (n.right == null && n.left == null) return s*10+n.val;
+        return sum(n.left, s*10+n.val) + sum(n.right, s*10+n.val);
+    }
+}

f5f70503b4743a4f3683a99e1a7e6ef64e23ebdf
Yonezcy <364251097@qq.com>
2018-02-18 17:53:25 +0800

    增加2月16日算法，在链表中删除某个结点

--- Delete Node in a Linked List.java
+++ Delete Node in a Linked List.java
@@ -1,5 +1,5 @@
-// leetcode 143 problem
-// 对链表进行前后依次取数，重新排序
+// leetcode 237 problem
+// 在链表中删除某个结点
 
 /**
  * Definition for singly-linked list.
@@ -9,37 +9,11 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+// 由于找不到前一个结点，只能将后一个结点的值赋给待删除结点，删除后一个结点
 class Solution {
-    public void reorderList(ListNode head) {
-        if (head == null || head.next == null) return;
-            
-        //Find the middle of the list
-        ListNode p1 = head;
-        ListNode p2 = head;
-        while (p2.next != null && p2.next.next != null) { 
-            p1 = p1.next;
-            p2 = p2.next.next;
-        }
-            
-        //Reverse the half after middle  1->2->3->4->5->6 to 1->2->3->6->5->4
-        ListNode preMiddle = p1;
-        ListNode preCurrent = p1.next;
-        while (preCurrent.next != null) {
-            ListNode current = preCurrent.next;
-            preCurrent.next = current.next;
-            current.next = preMiddle.next;
-            preMiddle.next = current;
-        }
-            
-        //Start reorder one by one  1->2->3->6->5->4 to 1->6->2->5->3->4
-        p1 = head;
-        p2 = preMiddle.next;
-        while (p1 != preMiddle) {
-            preMiddle.next = p2.next;
-            p2.next = p1.next;
-            p1.next = p2;
-            p1 = p2.next;
-            p2 = preMiddle.next;
-        }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }

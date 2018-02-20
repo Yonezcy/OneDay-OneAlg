2cb45439dd8fe259ef85f0a3231fc758a61a22d6
Yonezcy <364251097@qq.com>
2018-02-20 11:00:13 +0800

    增加2月20日算法，删除链表中值为val的结点

---
+++ Remove Linked List Elements.java
@@ -0,0 +1,27 @@
+// leetcode 203 problem
+// 删除链表中值为val的结点
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode fakeHead = new ListNode(-1);
+        fakeHead.next = head;
+        ListNode curr = head, prev = fakeHead;
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = prev.next;
+            }
+            curr = curr.next;
+        }
+        return fakeHead.next;
+    }
+}

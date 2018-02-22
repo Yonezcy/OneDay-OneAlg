8de1fa26b3a1db642a84b77621f7b58fddc02572
Yonezcy <364251097@qq.com>
2018-02-22 11:11:25 +0800

    增加2月22日算法，判断一个链表是否是回文链表

---
+++ Palindrome Linked List.java
@@ -0,0 +1,47 @@
+// leetcode 234
+// 判断一个链表是否是回文链表
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// 找到中间那个结点，将后面的所有结点reverse，判断前一半和后一半是否equal
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { 
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}

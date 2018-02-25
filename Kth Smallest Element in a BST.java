bd9f3e9f568c52778c4ec30e9d0dc14f85996e2b
Yonezcy <364251097@qq.com>
2018-02-25 10:33:13 +0800

    增加2月25日算法，找出二叉树中第k小的元素

---
+++ Kth Smallest Element in a BST.java
@@ -0,0 +1,28 @@
+// leetcode 230 problem
+// 找出二叉树中第k小的元素
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+     public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+        return root.val;
+    }
+    
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+}

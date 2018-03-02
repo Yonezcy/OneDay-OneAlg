cd7b3d4adcefe0edb203ec4f1d4132651776417a
Yonezcy <364251097@qq.com>
2018-03-02 13:06:44 +0800

    增加3月2日算法，判断一个整型数是否是3的n次方

---
+++ Power of Three.java
@@ -0,0 +1,9 @@
+// leetcode 336 problem
+// 判断一个整型数是否是3的n次方
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        // 1162261467 is 3^19,  3^20 is bigger than int  
+        return ( n>0 &&  1162261467%n==0);
+    }
+}

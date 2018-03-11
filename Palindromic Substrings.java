7f3aa4afc40f38c8db7c6892cac21bd3c77f5775
Yonezcy <364251097@qq.com>
2018-03-11 14:25:06 +0800

    找出字符数组中回文子串的个数

--- Palindromic Substrings.java
+++ Palindromic Substrings.java
@@ -8,7 +8,7 @@ public class Solution {
         if (s == null || s.length() == 0) return 0;
         for (int i = 0; i < s.length(); i++) { 
             extendPalindrome(s, i, i); 
-            extendPalindrome(s, i, i + 1); 
+            extendPalindrome(s, i, i+1); 
         }
         return count;
     }

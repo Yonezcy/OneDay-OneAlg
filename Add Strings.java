99ef70dd23a845572d2df8e2eefb76bd8bac9321
Yonezcy <364251097@qq.com>
2018-03-17 11:05:29 +0800

    增加3月17日算法，求两个string类型的整形数相加后的结果（不能把输入直接转化为int）

---
+++ Add Strings.java
@@ -0,0 +1,27 @@
+// leetcode 415 problem
+// 求两个string类型的整形数相加后的结果（不能把输入直接转化为int）
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder res = new StringBuilder();
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = 0;
+            if (i >= 0) {
+                sum += (num1.charAt(i) - '0');
+                i--;
+            }
+            if (j >= 0) {
+                sum += (num2.charAt(j) - '0');
+                j--;
+            }
+            sum += carry; 
+            carry = sum / 10;
+            sum = sum % 10;
+            res.append(sum);
+        }
+        return res.reverse().toString();
+    }
+}

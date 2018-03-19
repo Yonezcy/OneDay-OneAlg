b2bd3e620cb0c43a6861cfa11017e8031f5e3ecd
Yonezcy <364251097@qq.com>
2018-03-19 11:00:27 +0800

    增加3月19日算法，将十进制整形数转换为16进制字符串

---
+++ Convert a Number to Hexadecimal.java
@@ -0,0 +1,16 @@
+// leetcode 405 problem
+// 将十进制整形数转换为16进制字符串
+
+class Solution {
+    char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    public String toHex(int num) {
+        if (num == 0) return "0";
+        String result = "";
+        while (num != 0) {
+            result = map[(num & 15)] + result; 
+            // >>> 高位补0；>> 高位补上原来的高位，左移没有 <<< 因为不存在符号问题
+            num = (num >>> 4);
+        }
+        return result;
+    }
+}

7c6d9b4c31502223a65ddd7f49aa958cd2e5c67c
Yonezcy <364251097@qq.com>
2018-03-23 11:15:55 +0800

    增加3月23日算法，找出二叉树中出现次数最多的结点

---
+++ Find Mode in Binary Search Tree.java
@@ -0,0 +1,47 @@
+// leetcode 501 problem
+// 找出二叉树中出现次数最多的结点
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public Integer prev = null;
+    public int count = 1;
+    public int max = 0;
+    public int[] findMode(TreeNode root) {
+        if (root == null) return new int[0];
+        
+        List<Integer> list = new ArrayList<>();
+        traverse(root, list);
+        
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) res[i] = list.get(i);
+        return res;
+    }
+    
+    private void traverse(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        traverse(root.left, list);
+        if (prev != null) {
+            if (root.val == prev)
+                count++;
+            else
+                count = 1;
+        }
+        if (count > max) {
+            max = count;
+            list.clear();
+            list.add(root.val);
+        } else if (count == max) {
+            list.add(root.val);
+        }
+        prev = root.val;
+        traverse(root.right, list);
+    }
+}

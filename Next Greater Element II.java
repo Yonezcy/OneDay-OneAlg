bb31721cf5e2d1cb26cd8742d7846ca9c18ee8cf
Yonezcy <364251097@qq.com>
2018-03-30 11:17:33 +0800

    增加3月30日算法，找出数组中比所有数大的第一个数（using stack）

---
+++ Next Greater Element II.java
@@ -0,0 +1,17 @@
+// leetcode 503 problem
+// 找出数组中比所有数大的第一个数（using stack）
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length, next[] = new int[n];
+        Arrays.fill(next, -1);
+        Stack<Integer> stack = new Stack<>(); // index stack
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n]; 
+            while (!stack.isEmpty() && nums[stack.peek()] < num)
+                next[stack.pop()] = num;
+            if (i < n) stack.push(i);
+        }   
+        return next;
+    }
+}

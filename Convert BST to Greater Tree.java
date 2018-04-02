2e04fb1fddaee192803682591076b3bf0832819f
Yonezcy <364251097@qq.com>
2018-04-02 10:35:09 +0800

    增加4月2日算法，将一颗二叉查询树转换为greater tree（所有结点的值加上比它大的所有结点的值形成的树）

---
+++ Convert BST to Greater Tree.java
@@ -0,0 +1,17 @@
+// leetcode 538 problem
+// 将一颗二叉查询树转换为greater tree（所有结点的值加上比它大的所有结点的值形成的树）
+
+class Solution {
+    public int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
+    public void convert(TreeNode cur) {
+        if (cur == null) return;
+        convert(cur.right);
+        cur.val += sum;
+        sum = cur.val;
+        convert(cur.left);
+    }
+}

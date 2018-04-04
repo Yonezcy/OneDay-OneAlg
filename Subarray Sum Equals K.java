698ce3db31da23e34f3635415f4b97ed43b065e3
Yonezcy <364251097@qq.com>
2018-04-04 11:32:27 +0800

    增加4月4日算法，在数组中找出和为k的子串的个数

---
+++ Subarray Sum Equals K.java
@@ -0,0 +1,22 @@
+// leetcode 560 problem
+// 在数组中找出和为k的子串的个数
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            
+            // 如果之前有存在presum且presum+k=sum，说明遍历至此一定存在和为k的子序列，数量取决于presum的数量
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return result;
+    }
+}

5f038d8389544153618ec18a3f3b917efb1528cf
Yonezcy <364251097@qq.com>
2018-04-06 13:28:36 +0800

    增加4月6日算法，找出一个数组的最短子串使得子串排序后整个数组呈升序

---
+++ Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,15 @@
+// leetcode 581 problem
+// 找出一个数组的最短子串使得子串排序后整个数组呈升序
+
+class Solution {
+    public int findUnsortedSubarray(int[] A) {
+        int n = A.length, begin = -1, end = -2, min = A[n-1], max = A[0];
+        for (int i=1;i<n;i++) {
+          max = Math.max(max, A[i]);
+          min = Math.min(min, A[n-1-i]);
+          if (A[i] < max) end = i;
+          if (A[n-1-i] > min) begin = n-1-i; 
+        }
+        return end - begin + 1;
+    }
+}

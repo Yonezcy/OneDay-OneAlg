39c450f592e982a4fe137ee6c7db25ec7e25a5fa
Yonezcy <364251097@qq.com>
2018-04-08 10:59:31 +0800

    增加4月8日算法，求数组中最长和谐子序列（最大最小元素的差为1）的长度

---
+++ Longest Harmonious Subsequence.java
@@ -0,0 +1,18 @@
+// leetcode 594 problem
+// 求数组中最长和谐子序列（最大最小元素的差为1）的长度
+
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Long, Integer> map = new HashMap<>();
+        for (long num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int result = 0;
+        for (long key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                result = Math.max(result, map.get(key + 1) + map.get(key));
+            }
+        }
+        return result;
+    }
+}

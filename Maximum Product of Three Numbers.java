3dbf75716ebbd08e79d65c9368b0695c2d3b21f8
Yonezcy <364251097@qq.com>
2018-04-11 09:12:53 +0800

    增加4月11日算法，求数组中三个数的最大乘积

--- Maximum Product of Three Numbers.java
+++ Maximum Product of Three Numbers.java
@@ -5,6 +5,7 @@ class Solution {
     public int maximumProduct(int[] nums) {
          Arrays.sort(nums);
          int a = nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3];
+         // 需要注意的是要考虑到负数情况
          int b = nums[0] * nums[1] * nums[nums.length-1];
          return a > b ? a : b;
     }

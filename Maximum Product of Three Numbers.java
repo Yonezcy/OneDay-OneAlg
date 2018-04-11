c10b54bc20d3d8929ab81464ef87e8dbd5b98522
Yonezcy <364251097@qq.com>
2018-04-11 09:12:18 +0800

    增加4月11日算法，求数组中三个数的最大乘积

---
+++ Maximum Product of Three Numbers.java
@@ -0,0 +1,11 @@
+// leetcode 628 problem
+// 求数组中三个数的最大乘积
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+         Arrays.sort(nums);
+         int a = nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3];
+         int b = nums[0] * nums[1] * nums[nums.length-1];
+         return a > b ? a : b;
+    }
+}

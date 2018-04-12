8e17a131c55d1ae921271765b416b654c6c1ef72
Yonezcy <364251097@qq.com>
2018-04-12 09:47:01 +0800

    增加4月12日算法，给定区间[L, R]，修剪二叉搜索树（剪掉val在区间外的结点）

---
+++ Trim a Binary Search Tree.java
@@ -0,0 +1,16 @@
+// leetcode 669 problem
+// 给定区间[L, R]，修剪二叉搜索树（剪掉val在区间外的结点）
+
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        
+        if (root.val < L) return trimBST(root.right, L, R);
+        if (root.val > R) return trimBST(root.left, L, R);
+        
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        
+        return root;
+    }
+}

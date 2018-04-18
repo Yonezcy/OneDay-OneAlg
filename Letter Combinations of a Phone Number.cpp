3a4f40e451122dbbb9801c28707dad1e9aed668f
Yonezcy <364251097@qq.com>
2018-04-18 09:29:26 +0800

    增加4月18日算法，手机按键的字母组合

---
+++ Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,22 @@
+// leetcode 17 problem
+// 手机按键的字母组合
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if (digits.size() == 0) return res;
+        string charmap[10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        res.push_back("");
+        for (int i = 0; i < digits.size(); i++)
+        {
+            vector<string> tempres;
+            string chars = charmap[digits[i] - '0'];
+            for (int c = 0; c < chars.size();c++)
+                for (int j = 0; j < res.size();j++)
+                    tempres.push_back(res[j]+chars[c]);
+            res = tempres;
+        }
+        return res;
+    }
+};

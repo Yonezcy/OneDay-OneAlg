a5a4bd5084fa82ffc8f1eedc4f0da624000c6662
Yonezcy <364251097@qq.com>
2018-04-19 11:15:24 +0800

    增加4月19日算法，在一个增序的旋转过的数组中搜索target

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,27 @@
+// leetcode 33 problem
+// 在一个增序的旋转过的数组中搜索target
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            int num = nums[mid];
+            // If nums[mid] and target are "on the same side" of nums[0], we just take nums[mid].
+            if ((nums[mid] < nums[0]) == (target < nums[0])) {
+                num = nums[mid];
+            } else {
+                num = target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            if (num < target)
+                lo = mid + 1;
+            else if (num > target)
+                hi = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}

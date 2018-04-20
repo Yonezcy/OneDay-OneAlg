1d58b0b35df39968417f19210f6a06432f3b1d57
Yonezcy <364251097@qq.com>
2018-04-20 16:02:01 +0800

    增加4月20日算法，判断一棵树是否是二叉搜索树

---
+++ Validate Binary Search Tree.java
@@ -0,0 +1,15 @@
+// leetcode 98 problem
+// 判断一棵树是否是二叉搜索树
+
+// 递归实现，只需要判断当前结点的值是否在上一代传下来的min和max之间
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}

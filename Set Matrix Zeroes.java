08fbf1a9024c83349fcdacbf020c87c88f2af43a
Yonezcy <364251097@qq.com>
2018-04-21 11:16:02 +0800

    增加4月21日算法，将矩阵0元素所在的行和列上的所有元素置0

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -1,36 +1,22 @@
 // leetcode 73 problem
-// 将二维数组中的0元素的同行同列的元素都置1
+// 将矩阵0元素所在的行和列上的所有元素置0
 
 class Solution {
-public void setZeroes(int[][] matrix) {
-    // fr标志第一行是否有0元素，fc标志第一列是否有0元素
-    boolean fr = false,fc = false;
-    for(int i = 0; i < matrix.length; i++) {
-        for(int j = 0; j < matrix[0].length; j++) {
-            if(matrix[i][j] == 0) {
-                if(i == 0) fr = true;
-                if(j == 0) fc = true;
-                // 第一行和第一列标志该列或该行是否有0元素
-                matrix[0][j] = 0;
-                matrix[i][0] = 0;
-            }
+    public void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
         }
-    }
-    for(int i = 1; i < matrix.length; i++) {
-        for(int j = 1; j < matrix[0].length; j++) {
-            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-    if(fr) {
-        for(int j = 0; j < matrix[0].length; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    if(fc) {
-        for(int i = 0; i < matrix.length; i++) {
-            matrix[i][0] = 0;
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
         }
     }
 }

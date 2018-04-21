86b80b07437f6bb9752c19ba90b49c42d86a5753
Yonezcy <364251097@qq.com>
2018-04-21 11:53:28 +0800

    增加4月21日算法，将矩阵0元素所在的行和列上的所有元素置0

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -3,20 +3,35 @@
 
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
-
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0) col0 = 0;
-            for (int j = 1; j < cols; j++)
-                if (matrix[i][j] == 0)
-                    matrix[i][0] = matrix[0][j] = 0;
+        boolean fr = false ,fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    // 设置fr fc的原因是，如果第一行和第一列本来没有0元素，但是由于第一行和第一列是标志位
+                    // 它们有可能被置0，导致后面的循环将整第一行或整第一列都置0，结果出错
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
         }
-
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 1; j--)
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
-            if (col0 == 0) matrix[i][0] = 0;
+                }
+            }
+        }
+        if (fr) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 }

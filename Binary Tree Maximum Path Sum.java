e41df8770e2cff4581bada35b3997f4b52b57bef
Yonezcy <364251097@qq.com>
2018-04-23 09:53:14 +0800

    增加4月23日算法，在二叉树中找出最大路径

---
+++ Binary Tree Maximum Path Sum.java
@@ -0,0 +1,22 @@
+// leetcode 124 problem
+// 在二叉树中找出最大路径
+
+class Solution {
+    int maxValue;
+    
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+    
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+
+        // 只能是单边路径，不能分叉
+        return Math.max(left, right) + node.val;
+    }
+}

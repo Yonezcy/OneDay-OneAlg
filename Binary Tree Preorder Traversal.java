133dafae3eb35783d6051d16b210afa8a14baf3d
Yonezcy <364251097@qq.com>
2018-04-25 09:40:25 +0800

    增加4月25日算法，求二叉树的前序遍历序列

---
+++ Binary Tree Preorder Traversal.java
@@ -0,0 +1,26 @@
+// leetcode 144 problem
+// 求二叉树的前序遍历序列
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> pre = new LinkedList<Integer>();
+		preHelper(root, pre);
+		return pre;
+	}
+    
+	public void preHelper(TreeNode root, List<Integer> pre) {
+		if (root == null) return;
+		pre.add(root.val);
+		preHelper(root.left, pre);
+		preHelper(root.right, pre);
+	}
+}

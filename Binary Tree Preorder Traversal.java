f9fedf392dfe22485f4b9bd418e697f26af90ebc
Yonezcy <364251097@qq.com>
2018-04-26 09:47:45 +0800

    增加4月26日算法，求二叉树的前序遍历序列（迭代解法）

--- Binary Tree Preorder Traversal.java
+++ Binary Tree Preorder Traversal.java
@@ -10,6 +10,8 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// Recursive solution
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
 		List<Integer> pre = new LinkedList<Integer>();
@@ -23,4 +25,22 @@ class Solution {
 		preHelper(root.left, pre);
 		preHelper(root.right, pre);
 	}
+}
+
+// iterative solution
+public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            result.add(p.val);  // Add before going to children
+            p = p.left;
+        } else {
+            TreeNode node = stack.pop();
+            p = node.right;   
+        }
+    }
+    return result;
 }

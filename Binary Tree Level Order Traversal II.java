b84f91af9edc1b94bb9a93439d9c0c41181f1b45
Yonezcy <364251097@qq.com>
2018-04-27 10:09:56 +0800

    增加4月27日算法，层序遍历二叉树（从下至上）

---
+++ Binary Tree Level Order Traversal II.java
@@ -0,0 +1,21 @@
+// leetcode 107 problem
+// 层序遍历二叉树（从下至上）
+
+// BFS Solution
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        levelMaker(wrapList, root, 0);
+        return wrapList;
+    }
+        
+    public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
+        if(root == null) return;
+        if(level >= list.size()) {
+            list.add(0, new LinkedList<Integer>());
+        }
+        levelMaker(list, root.left, level+1);
+        levelMaker(list, root.right, level+1);
+        list.get(list.size()-level-1).add(root.val);
+    }
+}

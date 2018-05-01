dcf952265a04e5d2362e126b1e5ae00a9a515ba7
Yonezcy <364251097@qq.com>
2018-05-01 10:04:46 +0800

    增加5月1日算法，计算完全二叉树的结点数

---
+++ Count Complete Tree Nodes.java
@@ -0,0 +1,14 @@
+// leetcode 222 problem
+// 计算完全二叉树的结点数
+
+class Solution {
+    int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
+    public int countNodes(TreeNode root) {
+        int h = height(root);
+        return h < 0 ? 0 :
+               height(root.right) == h-1 ? (1 << h) + countNodes(root.right)
+                                         : (1 << h-1) + countNodes(root.left);
+    }
+}

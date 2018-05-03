ee879bfd3e5b0bfff3289f29c79d9c5ba728473a
Yonezcy <364251097@qq.com>
2018-05-03 10:19:40 +0800

    增加5月3日算法，返回二叉树每一层最右边的结点集合

---
+++ Binary Tree Right Side View.java
@@ -0,0 +1,22 @@
+// leetcode 199 problem
+// 返回二叉树每一层最右边的结点
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if (curr == null) {
+            return;
+        }
+        if (currDepth == result.size()) {
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth+1);
+        rightView(curr.left, result, currDepth+1);
+    }
+}

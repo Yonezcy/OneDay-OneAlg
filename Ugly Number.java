73911caeb1298812777fe17a8d492df03eb14d7b
Yonezcy <364251097@qq.com>
2018-05-06 09:47:08 +0800

    增加5月6日算法，判断一个数是不是ugly number（ugly number是质因子只有2，3，5的正数）

---
+++ Ugly Number.java
@@ -0,0 +1,11 @@
+// leetcode 263 problem
+// 判断一个数是不是ugly number（ugly number是质因子只有2，3，5的正数）
+
+class Solution {
+    public boolean isUgly(int num) {
+        for (int i = 2; i < 6 && num > 0; i++)
+            while (num % i == 0)
+                num /= i;
+        return num == 1;
+    }
+}

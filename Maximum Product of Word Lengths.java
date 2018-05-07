f0324e0d5d987aea99147b72212c1658f90314f7
Yonezcy <364251097@qq.com>
2018-05-07 10:06:47 +0800

    增加5月7日算法，找出字符串数组中最大的两个字符串长度乘积（两个字符串不能有重复字符）

---
+++ Maximum Product of Word Lengths.java
@@ -0,0 +1,25 @@
+// leetcode 318 probelm
+// 找出字符串数组中最大的两个字符串长度乘积（两个字符串不能有重复字符）
+
+class Solution {
+    public int maxProduct(String[] words) {
+        if (words == null || words.length == 0) return 0;
+        int len = words.length;
+        int[] value = new int[len];
+        for (int i = 0; i < len; i++) {
+            String tmp = words[i];
+            value[i] = 0;
+            for (int j = 0; j < tmp.length(); j++) {
+                // 左移一位的原因是让最后一位表示'a'
+                value[i] |= 1 << (tmp.charAt(j) - 'a');
+            }
+        }
+        int maxProduct = 0;
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j < len; j++) {
+                if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
+                    maxProduct = words[i].length() * words[j].length();
+            }
+        return maxProduct;
+    }
+}

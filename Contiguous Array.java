11e126c7b6b200a8fe4bded08394c1df2a7d048d
Yonezcy <364251097@qq.com>
2018-05-13 11:19:25 +0800

    增加5月13日算法，找出数组的子串中0和1的个数相等的最大子串长度

---
+++ Contiguous Array.java
@@ -0,0 +1,27 @@
+// leetcode 525 problem
+// 找出数组的子串中0和1的个数相等的最大子串长度
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) nums[i] = -1;
+        }
+        
+        Map<Integer, Integer> sumToIndex = new HashMap<>();
+        sumToIndex.put(0, -1);
+        int sum = 0, max = 0;
+        
+        // if sum[i, j] == 0  there is equal 0 and 1 between i and j
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sumToIndex.containsKey(sum)) {
+                max = Math.max(max, i-sumToIndex.get(sum));
+            }
+            else {
+                sumToIndex.put(sum, i);
+            }
+        }
+        
+        return max;
+    }
+}

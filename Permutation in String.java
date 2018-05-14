f8a1d316b194dabb1a8fb177904c9e96674864cc
Yonezcy <364251097@qq.com>
2018-05-14 09:52:01 +0800

    增加5月14日算法，判断第二个字符串的子串是否包含第一个字符串的任意排列

---
+++ Permutation in String.java
@@ -0,0 +1,31 @@
+// leetcode 567 problem
+// 判断第二个字符串的子串是否包含第一个字符串的任意排列
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
+        
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZero(count)) return true;
+        
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZero(count)) return true;
+        }
+        
+        return false;
+    }
+    
+    private boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}

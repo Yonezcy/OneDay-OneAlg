5f044283bef0b83117ab436d00e785aef6613d42
Yonezcy <364251097@qq.com>
2018-05-19 09:47:45 +0800

    5月19日复习算法，// leetcode 7 problem

---
+++ Reverse Integer.java
@@ -0,0 +1,20 @@
+// leetcode 7 problem
+// 将一个整形数翻转(考虑溢出)
+
+class Solution {
+    public int reverse(int x)
+    {
+        int result = 0;
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            // 溢出的话前后结果就会不相等
+            if ((newResult - tail) / 10 != result)
+                return 0;
+            result = newResult;
+            x = x / 10;
+        }
+        return result;
+    }
+}

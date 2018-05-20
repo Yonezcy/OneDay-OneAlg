e77f60c9a65dfdd8de88271ec876b6e6b9369dcb
Yonezcy <364251097@qq.com>
2018-05-20 09:43:24 +0800

    5月20日复习算法，判断一个整形数是否是回文数

--- Palindrome Number.cpp
+++ Palindrome Number.cpp
@@ -4,23 +4,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //negative integer is not palindrome number!
-        return (x>=0 && reverse(x)==x);
+        // negative integer is not palindrome number!
+        return (x >= 0 && reverse(x) == x);
     }
     int reverse(int x) {
-            long long res = 0;
-            bool isPositive = true;
-            if (x < 0){
-                isPositive = false;
-                x *= -1;
-            }
-
-            while (x > 0){
-                res = res * 10 + x % 10;
-                x /= 10;
-            }
-            if (res > INT_MAX) return 0;
-            if (isPositive) return res;
-            else return -res;
+        long long res = 0;
+        bool isPositive = true;
+        if (x < 0) {
+            isPositive = false;
+            x *= -1;
         }
+
+        while (x > 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        if (res > INT_MAX) return 0;
+        if (isPositive) return res;
+        else return -res;
+    }
 };

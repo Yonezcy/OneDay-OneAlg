2805feadcfb994f377e8d4df2112b093d3cba7be
Yonezcy <364251097@qq.com>
2018-05-22 19:25:41 +0800

    5月22日复习算法，实现正则表达式匹配

--- Regular Expression Matching.java
+++ Regular Expression Matching.java
@@ -8,7 +8,7 @@
    here are two sub conditions:
                1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
                2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
-                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
+                              dp[i][j] = dp[i-1][j]   // in this case, a* counts as multiple a 
                            or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
                            or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
 '''

d8707e2f6d1b7ccfd12489c8453d10f49eb53323
Yonezcy <364251097@qq.com>
2018-05-23 09:37:45 +0800

    5月23日复习算法，Container With Most Water

--- Container With Most Water.java
+++ Container With Most Water.java
@@ -1,11 +1,14 @@
+// leetcode 11 problem
+// Container With Most Water
+
 class Solution {
-    //brute force
+    // brute force
     public int maxArea(int[] height) {
         int i, j;
         int maxVolume = 0;
-        for (i=0; i<height.length-1; i++){
-            for (j=i+1; j<height.length; j++){
-                if (height[i] >= height[j]){
+        for (i = 0; i < height.length-1; i++) {
+            for (j = i+1; j < height.length; j++) {
+                if (height[i] >= height[j]) {
                     maxVolume = Math.max(height[j] * (j-i), maxVolume);
                 }
                 else {
@@ -15,8 +18,8 @@ class Solution {
         }
         return maxVolume;
     }
-    //two pointer approach
-    public int maxArea_2(int[] height) {
+    // two pointer approach
+    public int maxArea(int[] height) {
         int maxarea = 0, l = 0; r = height.length - 1;
         while (l < r) {
             maxarea =  Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));

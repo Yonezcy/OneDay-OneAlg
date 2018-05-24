9b8081ef9d346ff5832e474bb8702b9a32dc5e2f
Yonezcy <364251097@qq.com>
2018-05-24 10:52:14 +0800

    5月24日复习算法，找出字符数组中字符串的最长公共前缀

--- Longest Common Prefix.java
+++ Longest Common Prefix.java
@@ -2,14 +2,14 @@
 // 找出字符数组中字符串的最长公共前缀
 
 class Solution {
-    //brute force
+    // brute force
     public String longestCommonPrefix(String[] strs) {
         int i;
         int maxLength = 0;
         if (strs.length < 2) return " ";
         
         String s3 = strs[1].substring(0, findLengthOfTwoString(strs[0], strs[1]));
-        for (i=2; i<strs.length; i++){
+        for (i = 2; i < strs.length; i++){
             maxLength = Math.max(maxLength, findLengthOfTwoString(s3, strs[i]));
             s3 = strs[i].substring(0, maxLength);
         }
@@ -19,8 +19,8 @@ class Solution {
         int maxLength = 0;
         int i;
         int len = Math.min(s1.length(), s2.length());
-        for (i=0; i<len; i++){
-            if (s1.charAt(i) == s2.charAt(i)){
+        for (i = 0; i < len; i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
                 maxLength++;
             }
             else break;
@@ -34,7 +34,7 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
         String prefix = strs[0];
-        for (int i=1; i<strs.length; i++)
+        for (int i = 1; i < strs.length; i++)
             // function indexOf() return the first index of prefix in strs[i]
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length()-1);
@@ -48,13 +48,13 @@ class Solution {
     // vertical scanning
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-        for (int i=0; i<strs[0].length; i++) {
+        for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
-            for (int j=i; j<strs.length; j++) {
-                if (i == strs[j].length || strs[j].charAt(i) != c)
+            for (int j = i; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
                     return strs[0].substring(0, i);
             }
         }
-        return 0;
+        return "";
     }
 }

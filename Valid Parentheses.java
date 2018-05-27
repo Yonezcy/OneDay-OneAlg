be271e744d4683f36a98b35cd5f063dbcb2ecf2d
Yonezcy <364251097@qq.com>
2018-05-27 11:27:38 +0800

    5月27日复习算法，判断字符串是否为合理的括号对

--- Valid Parentheses.java
+++ Valid Parentheses.java
@@ -1,48 +1,19 @@
+// leetcode 20 problem
+// 判断字符串是否为合理的括号对
+
 class Solution {
     public boolean isValid(String s) {
-        int len = s.length();
-		boolean isvalid = true;
-		Stack<Character> stack = new Stack<Character>();
-		Character[] all = {'[','(','{',']',')','}'};
-		Character[] left = {']',')','}'};
-		List<Character> leftlist = Arrays.asList(left);
-		List<Character> alllist = Arrays.asList(all);
-		
-		Map<Character, Character> map = new HashMap<Character, Character>();
-		map.put('[', ']');
-		map.put('(', ')');
-		map.put('{', '}');
-		map.put('}', '{');
-		map.put(')', '(');
-		map.put(']', '[');
-		
-		LinkedList<Character> linked = new LinkedList<Character>();
-		for (int i = 0; i < len; i++) {
-			if(alllist.contains(s.charAt(i))){
-				linked.add(s.charAt(i));
-			}
-		}
-		
-		if(linked.size()==0) return  true;
-		stack.push(linked.get(0));
-		
-		for (int i = 1; i < linked.size(); i++) {
-			Character str = linked.get(i);
-			if(leftlist.contains(str) && stack.size() > 0){
-				if(map.get(str).equals(stack.peek())){
-					stack.pop();
-				}else{
-					isvalid = false;
-				}
-			}else{
-				stack.push(str);
-			}
-		}
-		
-		if(stack!=null && stack.size()>0){
-			isvalid = false;
-		}
-		
-		return isvalid;
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
     }
 }

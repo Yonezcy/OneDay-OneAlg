b2fa82dc6a97b4d747fcb43806e3e45cd8301052
Yonezcy <364251097@qq.com>
2018-06-12 10:28:31 +0800

    6月12日复习算法，求数组中和为target的所有序列

--- Combination Sum.java
+++ Combination Sum.java
@@ -11,13 +11,13 @@ class Solution {
     }
 
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-        if(remain < 0) return;
-        else if(remain == 0) list.add(new ArrayList<>(tempList));
-        else{ 
-            for(int i = start; i < nums.length; i++){
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else { 
+            for (int i = start; i < nums.length; i++) {
                 tempList.add(nums[i]);
-                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-                tempList.remove(tempList.size() - 1);
+                backtrack(list, tempList, nums, remain-nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size()-1);
             }
         }
     }

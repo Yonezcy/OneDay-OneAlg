1d41e04177e9b4754aeb4f4a32db69e3283e2f31
Yonezcy <364251097@qq.com>
2018-06-29 15:23:13 +0800

    6月29日复习算法，实现通配符的匹配机制

--- Wildcard Matching.java
+++ Wildcard Matching.java
@@ -11,7 +11,8 @@
 
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length(), n = p.length();
+        int m = s.length();
+        int n = p.length();
         boolean[][] dp = new boolean[m+1][n+1];
         dp[0][0] = true;
         for (int i = 1; i <= m; i++) {

65b26b8a30258778f71b38b81522f3c48416cf75
Yonezcy <364251097@qq.com>
2018-07-01 16:16:26 +0800

    7月1日复习算法，求全排列（给定数组中有重复）

--- Permutations II.java
+++ Permutations II.java
@@ -9,17 +9,17 @@ public List<List<Integer>> permuteUnique(int[] nums) {
     return list;
 }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
-    if(tempList.size() == nums.length){
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, boolean[] used){
+    if (tempList.size() == nums.length) {
         list.add(new ArrayList<>(tempList));
-    } else{
-        for(int i = 0; i < nums.length; i++){
-            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+    } else {
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
             used[i] = true; 
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, used);
             used[i] = false; 
-            tempList.remove(tempList.size() - 1);
+            tempList.remove(tempList.size()-1);
         }
     }
 }

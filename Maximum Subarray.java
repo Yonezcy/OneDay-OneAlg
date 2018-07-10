2a3c34e877146484cb5288b372c2da61440628f8
Yonezcy <364251097@qq.com>
2018-07-10 12:54:19 +0800

    7月10日复习算法，求数组最大子序列和

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -5,7 +5,7 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int preSum, totalSum;
         preSum = totalSum = nums[0];
-        for (int i=1; i<nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
             if (preSum < 0) preSum = nums[i];
             else preSum += nums[i];
             if (preSum > totalSum) totalSum = preSum;

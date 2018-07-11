b82b9a3210f9f045afd77ceb27965045a3606ed3
Yonezcy <364251097@qq.com>
2018-07-11 12:41:48 +0800

    7月11日复习算法，Jump Game

--- Jump Game.java
+++ Jump Game.java
@@ -1,9 +1,10 @@
 // leetcode 55th problem
 // Jump Game
+
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length, index = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n-2; i >= 0; i--) {
             if (i + nums[i] >= index)
                 index = i;
         }

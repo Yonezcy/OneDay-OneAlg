d93aec9255d4c3489818027fdcd1e1cc7ae8b913
Yonezcy <364251097@qq.com>
2018-07-13 21:30:44 +0800

    7月13日复习算法，Unique Paths

--- Unique Paths(DP).java
+++ Unique Paths(DP).java
@@ -1,15 +1,16 @@
+// leetcode 62 problem
+// Unique Paths
+
 class Solution {
     public int uniquePaths(int m, int n) {
         // map[i][j]是从出发点能够走到当前点(i, j)的所有路径之和
         Integer[][] map = new Integer[m][n];
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < m; i++)
             map[i][0] = 1;
-        }
-        for(int j=0; j<n; j++){
+        for (int j = 0; j < n; j++)
             map[0][j] = 1;
-        }
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
+        for (int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
                 map[i][j] = map[i-1][j] + map[i][j-1];
             }
         }

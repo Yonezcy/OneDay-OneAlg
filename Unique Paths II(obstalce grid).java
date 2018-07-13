a6ab3643350f13cd92f9268548836451cd2cb95c
Yonezcy <364251097@qq.com>
2018-07-13 21:28:50 +0800

    7月13日复习算法，Unique Paths

--- Unique Paths II(obstalce grid).java
+++ Unique Paths II(obstalce grid).java
@@ -1,11 +1,14 @@
+// leetcode 62 problem
+// Unique Paths
+
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid.length == 0) return 0;
         int rows = obstacleGrid.length;
         int cols = obstacleGrid[0].length;
         
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (obstacleGrid[i][j] == 1)
                     obstacleGrid[i][j] = 0;
                 else if (i == 0 && j == 0)

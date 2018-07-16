d8d6a30916a3f816355740100f5a8756e09372a6
Yonezcy <364251097@qq.com>
2018-07-16 17:08:42 +0800

    7月16日复习算法，找出两数之和为target的排列数量

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -1,11 +1,15 @@
+// leetcode 70 problem
+// 找出两数之和为target的排列数量
+
 class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) return n; 
         
         int count[] = new int[n+1];
         count[0] = 0; count[1] = 1; count[2] = 2;
+
         // 从3开始，因为2是特例，count[2] != count[0] + count[1]
-        for (int i=3; i<n+1; i++) {
+        for (int i = 3; i < n+1; i++) {
             count[i] = count[i-1] + count[i-2];
         }
         return count[n];

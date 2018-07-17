a896f29d6a25f3a4abc8b501be3b77fd8e35963d
Yonezcy <364251097@qq.com>
2018-07-17 10:47:55 +0800

    7月17日复习算法，将矩阵0元素所在的行和列上的所有元素置0

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -3,7 +3,7 @@
 
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean fr = false ,fc = false;
+        boolean fr = false, fc = false;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {

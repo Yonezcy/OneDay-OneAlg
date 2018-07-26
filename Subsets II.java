58e699fb4a1a3377393c90a091cbb0cac66d51ce
Yonezcy <364251097@qq.com>
2018-07-26 16:00:56 +0800

    7月26日复习算法，求一维数组的所有子集（给的数组中有重复）

--- Subsets II.java
+++ Subsets II.java
@@ -12,8 +12,8 @@ class Solution {
 
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
         list.add(new ArrayList<>(tempList));
-        for(int i = start; i < nums.length; i++){
-            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i-1]) continue; // skip duplicates
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, i+1);
             tempList.remove(tempList.size()-1);

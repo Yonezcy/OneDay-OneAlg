73208b2dc7ec6aa743b45d38614804d89a261ea5
Yonezcy <364251097@qq.com>
2018-08-02 08:14:12 +0800

    8月2日复习算法，层序遍历二叉树

--- Binary Tree Level Order Traversal.java
+++ Binary Tree Level Order Traversal.java
@@ -1,15 +1,6 @@
 // leetcode 102 problem
 // 层序遍历二叉树
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();

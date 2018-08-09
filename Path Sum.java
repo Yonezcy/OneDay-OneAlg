a810263986d05bd24f1e5477226d17ccf2b7d768
Yonezcy <364251097@qq.com>
2018-08-09 23:08:32 +0800

    8月9日复习算法，判断二叉树是否有从根到叶子和为target的路径

--- Path Sum.java
+++ Path Sum.java
@@ -1,20 +1,11 @@
 // leetcode 112 problem
 // 判断二叉树是否有从根到叶子和为target的路径
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
-        if (root.left == null && root.right == null && root.val == sum) return true;
         
+        if (root.left == null && root.right == null && root.val == sum) return true;
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
     }
 }

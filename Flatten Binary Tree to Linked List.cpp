3da3163237decf2666aa840f641386616ced8aa8
Yonezcy <364251097@qq.com>
2018-08-10 11:22:43 +0800

    8月10日复习算法，将二叉树变为链表（递归实现）

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -1,14 +1,6 @@
 // leetcode 114th problem
 // 将二叉树变为链表（递归实现）
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Solution {
 public:
     void flatten(TreeNode *root) {
@@ -20,5 +12,5 @@ private:
         root->right = flatten(root->left, flatten(root->right, tail));
         root->left = NULL;
         return root;
-    }
+    }	
 };

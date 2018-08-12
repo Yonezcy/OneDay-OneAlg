12e988ac298a21fe3ac6b7565059ab43ff17d6c6
Yonezcy <364251097@qq.com>
2018-08-12 22:09:01 +0800

    8月12日复习算法，杨辉三角问题java实现（dp）

--- Pascal's Triangle.java
+++ Pascal's Triangle.java
@@ -1,14 +1,13 @@
 // leetcode 118th problem
 // 杨辉三角问题java实现（dp）
+
 class Solution {
-    public List<List<Integer>> generate(int numRows)
-    {
+    public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> allrows = new ArrayList<List<Integer>>();
         ArrayList<Integer> row = new ArrayList<Integer>();
-        for(int i=0; i<numRows; i++)
-        {
+        for(int i = 0; i < numRows; i++) {
             row.add(0, 1);
-            for(int j=1; j<row.size()-1; j++)
+            for(int j = 1; j < row.size()-1; j++)
                 row.set(j, row.get(j)+row.get(j+1));
             allrows.add(new ArrayList<Integer>(row));
         }

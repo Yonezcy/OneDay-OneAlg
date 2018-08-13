e7a9193e522a35e104af75d59e61462c39c9feb2
Yonezcy <364251097@qq.com>
2018-08-13 15:16:36 +0800

    8月13日复习算法，杨辉三角问题dp实现（返回某一行）

--- Pascal's Triangle II.java
+++ Pascal's Triangle II.java
@@ -1,12 +1,12 @@
 // leetcode 119th problem
 // 杨辉三角问题dp实现（返回某一行）
+
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         ArrayList<Integer> row = new ArrayList<Integer>();
-        for(int i=0; i<=rowIndex; i++)
-        {
+        for(int i = 0; i <= rowIndex; i++) {
             row.add(0, 1);
-            for(int j=1; j<row.size()-1; j++)
+            for(int j = 1; j < row.size()-1; j++)
                 row.set(j, row.get(j)+row.get(j+1));
         }
         return row;

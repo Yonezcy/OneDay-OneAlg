d1c97ea490a139da3b34bf834dcd904584a4e3f8
Yonezcy <364251097@qq.com>
2018-08-14 11:10:18 +0800

    8月14日复习算法，找出三角形从上到下的最小数字和（只能对相邻元素求和）

--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,12 @@
 // leetcode 120th problem
+// 找出三角形从上到下的最小数字和（只能对相邻元素求和）
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int[][] A = new int[triangle.size()+1][triangle.size()+1];
         // 从下至上，dp解法；若从上至下，要考虑很多边界问题
-        for(int i=triangle.size()-1; i>=0; i--) {
-            for(int j=0; j<triangle.get(i).size(); j++) {
+        for (int i = triangle.size()-1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
                 A[i][j] = Math.min(A[i+1][j], A[i+1][j+1]) + triangle.get(i).get(j);
             }
         }

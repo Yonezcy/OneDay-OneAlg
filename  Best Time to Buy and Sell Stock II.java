c040f4e939463aefbf7d7c909d2c8580e9d772a2
Yonezcy <364251097@qq.com>
2018-08-16 10:27:25 +0800

    8月16日复习算法，给定一个数组，找出多组后数减前数的差的最大和

---  Best Time to Buy and Sell Stock II.java
+++  Best Time to Buy and Sell Stock II.java
@@ -1,16 +1,12 @@
 // leetcode 122th problem
-// Say you have an array for which the ith element is the price of a given stock on day
-// Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
-// (ie, buy one and sell one share of the stock multiple times). 
-// However, you may not engage in multiple transactions at the same time 
-// (ie, you must sell the stock before you buy again).
+// 给定一个数组，找出多组后数减前数的差的最大和
 
 class Solution {
     public int maxProfit(int[] prices) {
         int total = 0;
-        for (int i=0; i<prices.length-1; i++) {
+        for (int i = 0; i < prices.length-1; i++) {
         	// Given an array 1 3 2 4   (3-1)+(4-2)>(4-1) because 3>2
-            if (prices[i+1]>prices[i]) total += prices[i+1] - prices[i];
+            if (prices[i+1] > prices[i]) total += prices[i+1] - prices[i];
         }
         return total;
     }

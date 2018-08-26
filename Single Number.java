7adf1e132aa9e04e523d254abfc7b0520cea6c7c
Yonezcy <364251097@qq.com>
2018-08-26 13:40:36 +0800

    8月26日复习算法，数组中所有数字出现两次，只有一个出现一次，求出它

--- Single Number.java
+++ Single Number.java
@@ -4,11 +4,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
         if (nums.length == 0) return 0;
-        
         int res = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++)
             res ^= nums[i]; 
-        }
         return res;
     }
 }

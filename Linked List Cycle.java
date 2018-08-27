1d0185796773c5a8832e7632adfd065ddc7b6b7a
Yonezcy <364251097@qq.com>
2018-08-27 09:59:17 +0800

    8月27日复习算法，判断一个链表中是否有循环

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -1,28 +1,17 @@
 // leetcode 141 problem
 // 判断一个链表中是否有循环
 
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null) return false;
+        if (head == null) return false;
 
         // 设置两个指针，runner每次移动两步，walker一步，如果有循环总会有相遇的时候
         ListNode walker = head;
         ListNode runner = head;
-        while(runner.next!=null && runner.next.next!=null) {
+        while (runner.next != null && runner.next.next != null) {
             walker = walker.next;
             runner = runner.next.next;
-            if(walker==runner) return true;
+            if (walker == runner) return true;
         }
         return false;
     }

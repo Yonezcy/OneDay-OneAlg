8905ad302944d75acb5ebf8c031e2c9a96e3992d
Yonezcy <364251097@qq.com>
2018-08-29 10:02:14 +0800

    8月29日复习算法，求二叉树的后序遍历序列

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -1,16 +1,6 @@
 // leetcode 145 problem
 // 求二叉树的后序遍历序列
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
 // Recursive solution
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -27,13 +17,13 @@ class Solution {
 	}
 }
 
-//Iterative solution
+// Iterative solution
 public List<Integer> postorderTraversal(TreeNode root) {
     LinkedList<Integer> result = new LinkedList<>();
     Deque<TreeNode> stack = new ArrayDeque<>();
     TreeNode p = root;
-    while(!stack.isEmpty() || p != null) {
-        if(p != null) {
+    while (!stack.isEmpty() || p != null) {
+        if (p != null) {
             stack.push(p);
             result.addFirst(p.val);  // Reverse the process of preorder
             p = p.right;             // Reverse the process of preorder

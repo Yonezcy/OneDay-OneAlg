3e754de4a17bbce45a6fa37d4476f4bd2ed7fdb1
Yonezcy <364251097@qq.com>
2018-08-31 10:52:52 +0800

    8月31日复习算法，将增序数组的后一部分移到前面，找出最小的元素

--- Find Minimum in Rotated Sorted Array.java
+++ Find Minimum in Rotated Sorted Array.java
@@ -4,20 +4,16 @@
 class Solution {
     public int findMin(int[] nums) {
         int start = 0, end = nums.length - 1;
-        
         while (start < end) {
             if (nums[start] < nums[end])
                 return nums[start];
-            
             int mid = (start + end) / 2;
-            
             if (nums[mid] >= nums[start]) {
-                start = mid+1;
+                start = mid + 1;
             } else {
                 end = mid;
             }
         }
-        
         return nums[start];
     }
 }

bf93608b9e2a86635c906ee6d5ac54470b7ed4c7
Yonezcy <364251097@qq.com>
2018-09-04 10:17:41 +0800

    9月4日复习算法，寻找数组中的主元（出现次数大于n／2的元素，主元始终存在）

--- Majority Element.java
+++ Majority Element.java
@@ -1,9 +1,27 @@
 // leetcode 169th problem
-// 寻找数组中的主元（出现次数大于n／2的元素）
+// 寻找数组中的主元（出现次数大于n／2的元素，主元始终存在）
 
+// Solution 1: sort
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
+    }
+}
+
+// Solution 2: count
+class Solution {
+    public int majorityElement(int[] num) {
+		int major = num[0], count = 1;
+        for (int i = 1; i < num.length; i++) {
+            if (count == 0) {
+                count++;
+                major = num[i];
+            } else if (major == num[i]) {
+                count++;
+            } else count--;
+            
+        }
+        return major;
     }
 }

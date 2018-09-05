a93dbc5f37b3c1bf3726a404558ef31080e1a48b
Yonezcy <364251097@qq.com>
2018-09-05 09:57:03 +0800

    9月5日复习算法，将数组中的数拼接起来，返回最大的数

--- Largest Number.py
+++ Largest Number.py
@@ -1,5 +1,5 @@
 # leetcode 179 problem
-# 将输入数组中的数拼接起来，返回最大的数
+# 将数组中的数拼接起来，返回最大的数
 
 class Solution:
     # @param num, a list of integers
@@ -8,6 +8,7 @@ class Solution:
         num = [str(x) for x in num]
         # lambda表达式定义一个简单的数组
         # cmp(y+x, x+y)降序排列 cmp(x+y, y+x)是升序
-        num.sort(cmp=lambda x, y: cmp(y+x, x+y))
+        # cmp前减后是升序，后减前是降序
+        num.sort(cmp = lambda x, y: cmp(y+x, x+y))
         # or '0' 防止输入为'0'的情况
         return ''.join(num).lstrip('0') or '0'

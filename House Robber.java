ff4c9abad98fae70cd7f1b00c0e2c91037d1f03f
Yonezcy <364251097@qq.com>
2018-09-07 10:19:32 +0800

    9月7日复习算法，求数组中不相邻元素的和的最大值

--- House Robber.java
+++ House Robber.java
@@ -1,15 +1,18 @@
 // leetcode 198th problem
-// House Robber dp solution
+// 求数组中不相邻元素的和的最大值
+
+// DP Solution
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if (n == 0) return 0;
         if (n == 1) return nums[0];
-        int dp[] = new int[n];
+
+        int dp[] = new int[n]; // Total sum.
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
         }
         return dp[n-1];
     }

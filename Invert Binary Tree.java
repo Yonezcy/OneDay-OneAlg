180bd1134681a7f01b77cb13b5eb65cd047783c4
Yonezcy <364251097@qq.com>
2018-09-17 11:17:19 +0800

    9月17日复习算法，将一颗二叉树反转

--- Invert Binary Tree.java
+++ Invert Binary Tree.java
@@ -3,24 +3,50 @@
 // Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a 
 // whiteboard
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// Recursive Solution.
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        
         TreeNode left = root.left;
         TreeNode right = root.right;
         root.left = invertTree(right);
-        root.right = invertTree(left);
-        
+        root.right = invertTree(left);       
+        return root;
+    }
+}
+
+// Iterative Solution(Using stack, same to recursive substantially)(DFS).
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+        return root;
+    }
+}
+
+// Level Order Tranversal(Using queue)(BFS).
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
         return root;
     }
 }

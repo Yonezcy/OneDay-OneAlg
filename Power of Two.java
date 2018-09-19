6b510ca3d569ef6d96bfb55a61f3214576373ff2
Yonezcy <364251097@qq.com>
2018-09-19 09:49:31 +0800

    9月19日复习算法，判断一个数是否为2的n次方

--- Power of Two.java
+++ Power of Two.java
@@ -4,7 +4,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) return false;
-        int a = n & (n-1);
+        // i.e. power(2, 3) = 1000, 1000-0001=0111, 1000 & 0111 = 0
+        int a = n & (n - 1);
         if (a == 0) return true;
         return false;
     }

f9bf62cd2ea6e7622218f901f97d3205e097093b
Yonezcy <364251097@qq.com>
2018-09-20 09:19:24 +0800

    9月20日增加算法，找出n以内的素数的数量

---
+++ Count Primes.java
@@ -0,0 +1,19 @@
+// leetcode 204 problem
+// 找出n以内的素数的数量
+
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                // 找出所有i的小于n的倍数置True
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        return count;
+    }
+}

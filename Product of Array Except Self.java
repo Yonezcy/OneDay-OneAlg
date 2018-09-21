b99e74f3abe629a47c2951d7f38343f3ddaea02a
Yonezcy <364251097@qq.com>
2018-09-21 10:12:19 +0800

    9月21日复习算法，给定一个数组num，输出一个相同维数的数组，每个元素等于除了num[i]之外所有元素的乘积(不能用除法)

--- Product of Array Except Self.java
+++ Product of Array Except Self.java
@@ -1,5 +1,5 @@
 // leetcode 238 problem
-// 给定一个数组num，输出一个相同维数的数组，每个元素等于除了num[i]之外所有元素的乘积
+// 给定一个数组num，输出一个相同维数的数组，每个元素等于除了num[i]之外所有元素的乘积(不能用除法)
 
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
@@ -8,10 +8,11 @@ public class Solution {
         res[0] = 1;
         // 求前n-1个元素的乘积
         for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+            res[i] = res[i-1] * nums[i-1];
         }
+
+        // 补上后面的元素乘积
         int right = 1;
-        // 再补上后面的元素
         for (int i = n - 1; i >= 0; i--) {
             res[i] *= right;
             right *= nums[i];

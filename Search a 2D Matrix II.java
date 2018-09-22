8536c1baadb265591cba49239eef28cda0891287
Yonezcy <364251097@qq.com>
2018-09-22 10:30:48 +0800

    9月22日复习算法，在行列递增的二维数组里查找某个数

--- Search a 2D Matrix II.java
+++ Search a 2D Matrix II.java
@@ -1,6 +1,7 @@
 // leetcode 240th problem
 // 在行列递增的二维数组里查找某个数
 
+// Solution1: Begin at right top corner.
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
@@ -15,6 +16,27 @@ class Solution {
                 col--;
             } else if (target > matrix[row][col]) {
                 row++;
+            }
+        }
+        return false;
+    }
+}
+
+// Solution2: Begin at left bottom corner.
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+            return false;
+        }
+        int col = 0;
+        int row = matrix.length - 1;
+        while (col <= matrix[0].length-1 && row >= 0) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                row--;
+            } else if (target > matrix[row][col]) {
+                col++;
             }
         }
         return false;

179f638313a9cf715d68294d43e4c4ba083069a9
Yonezcy <364251097@qq.com>
2018-09-23 12:13:18 +0800

    9月23日复习算法， 判断两个字符串是否是anagram（相同字母异序词）

--- Valid Anagram.java
+++ Valid Anagram.java
@@ -1,5 +1,5 @@
 // leetcode 242 problem
-// 判断一个由字母组成的字符串是否是由另一个字符串任意打乱顺序得到的
+// 判断两个字符串是否是anagram（相同字母异序词）
 
 class Solution {
     public boolean isAnagram(String s, String t) {
@@ -8,7 +8,7 @@ class Solution {
             alphabet[s.charAt(i) - 'a']++;
         for (int i = 0; i < t.length(); i++) 
             alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) 
+        for (int i: alphabet) 
             if (i != 0) return false;
         return true;
     }

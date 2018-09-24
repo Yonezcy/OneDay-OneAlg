cad28dca73f1bf4d16a6c66a2ffafb3326c46fb1
Yonezcy <364251097@qq.com>
2018-09-24 09:53:47 +0800

    9月24日复习算法，寻找二叉树所有根到叶子的路径

--- Binary Tree Paths.java
+++ Binary Tree Paths.java
@@ -1,26 +1,17 @@
 // leetcode 257 problem
-// 寻找二叉树所有的根到叶子的路径
+// 寻找二叉树所有根到叶子的路径
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> list = new ArrayList<String>();
-        if (root != null) 
-            searchBT(root, "", list);
+        if (root == null) return list;
+        searchBT(root, "", list);
         return list;
     }
     
     private void searchBT(TreeNode root, String path, List<String> list) {
         if (root.left == null && root.right == null) 
-            list.add(path+root.val);
+            list.add(path + root.val);
         if (root.left != null) 
             searchBT(root.left, path+root.val+"->", list);
         if (root.right != null) 

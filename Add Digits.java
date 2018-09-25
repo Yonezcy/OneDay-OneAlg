90a8e22028f88aabe952b87b8825a50be5c9e82f
Yonezcy <364251097@qq.com>
2018-09-25 09:49:08 +0800

    9月25日复习算法，将一个整形数的各位循环相加，直到结果为一位数为止

--- Add Digits.java
+++ Add Digits.java
@@ -1,14 +1,18 @@
 // leetcode 258 problem
 // 将一个整形数的各位循环相加，直到结果为一位数为止
 
-// O(1) solution
+// Output sequence for decimals (b = 10):
+// input: 0 1 2 3 4 ...
+// output: 0 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 ...
+
+// O(1) Solution.
 class Solution {
     public int addDigits(int num) {
         return 1 + (num - 1) % 9;
     }
 }
 
-// recursive solution
+// Recursive Solution.
 class Solution {
     public int addDigits(int num) {
         if (num % 10 == num) return num;

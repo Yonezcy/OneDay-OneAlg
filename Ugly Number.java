f37de5006a1776ac3691da17264f6f191acf7687
Yonezcy <364251097@qq.com>
2018-09-26 09:54:23 +0800

    9月26日复习算法，判断一个数是不是ugly number（ugly number是质因子只有2，3，5的正数）

--- Ugly Number.java
+++ Ugly Number.java
@@ -4,8 +4,9 @@
 class Solution {
     public boolean isUgly(int num) {
         for (int i = 2; i < 6 && num > 0; i++)
-            while (num % i == 0)
-                num /= i;
+            while (num % i == 0) {
+            	num /= i;
+            }
         return num == 1;
     }
 }

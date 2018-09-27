ffdf4e5001ddacc83aa8777936adb5f803726e4d
Yonezcy <364251097@qq.com>
2018-09-27 10:00:48 +0800

    9月27日复习算法，找出一个数能被分解成平方数的和的最小平方数的数量

--- Perfect Squares.java
+++ Perfect Squares.java
@@ -1,14 +1,16 @@
+// leetcode 279 problem
+// 找出一个数能被分解成平方数的和的最小平方数的数量
+
 class Solution {
-    // leetcode279 dp解法
     public int numSquares(int n) {
-        int dp[] = new int[n + 1];
+        int dp[] = new int[n+1];
         dp[0] = 0;
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i < n+1; i++) {
             int k = 1;
             int min = Integer.MAX_VALUE;;
             while (i - k * k >= 0) {
-                // 状态转移方程: dp[i] = min(dp[i-j*j]+1, min)
-                min = Math.min(dp[i-k*k] + 1, min);
+                // 状态转移方程: dp[i] = min(dp[j]+1, min) for j=[1, i-k^2]
+                min = Math.min(dp[i-k*k]+1, min);
                 k++;
             }
             dp[i] = min;

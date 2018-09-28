26665b251dcc8e29ec560fdcfc15f6ac3d5ce46c
Yonezcy <364251097@qq.com>
2018-09-28 09:46:49 +0800

    9月28日复习算法，将一维数组中的非零元素按原来的顺序移到0元素的前面

--- Move Zeroes.java
+++ Move Zeroes.java
@@ -4,11 +4,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         if (nums.length == 0) return;
-        
         int insertPosition = 0;
-        for (int num : nums) 
+        for (int num: nums) 
             if (num != 0) nums[insertPosition++] = num;
-        
         while (insertPosition < nums.length) 
             nums[insertPosition++] = 0;
     }

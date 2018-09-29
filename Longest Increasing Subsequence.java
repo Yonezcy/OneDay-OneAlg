714b746e860161ce36be80d8d62f761604025fa8
Yonezcy <364251097@qq.com>
2018-09-29 11:31:28 +0800

    9月29日复习算法，求数组最大递增子序列的长度

--- Longest Increasing Subsequence.java
+++ Longest Increasing Subsequence.java
@@ -1,17 +1,21 @@
+// leetcode 300 problem
+// 求数组最大递增子序列的长度
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if (nums.length == 0) return 0;
         int[] dp = new int[nums.length];
         dp[0] = 1;
         int maxans = 1;
-        for (int i=1; i<dp.length; i++) {
+        for (int i = 1; i < dp.length; i++) {
             int maxval = 0;
-            for (int j=0; j<i; j++) {
+            // 状态转移方程：dp[i] = max(dp[j]) + 1
+            // 找包含第i个数的最大递增子序列的长度
+            for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     maxval = Math.max(maxval, dp[j]);
                 }
             }
-            // 状态转移方程：dp[i] = max(dp[i]) + 1
             dp[i] = maxval + 1;
             maxans = Math.max(maxans, dp[i]);
         }

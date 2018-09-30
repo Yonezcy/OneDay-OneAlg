737ebf0527ebe1b939448813bf7423ac7cbed8a6
Yonezcy <364251097@qq.com>
2018-09-30 11:57:45 +0800

    9月30日复习算法，找出字符串数组中最大的两个字符串长度乘积（两个字符串不能有重复字符）

--- Maximum Product of Word Lengths.java
+++ Maximum Product of Word Lengths.java
@@ -5,7 +5,7 @@ class Solution {
     public int maxProduct(String[] words) {
         if (words == null || words.length == 0) return 0;
         int len = words.length;
-        int[] value = new int[len];
+        int value[] = new int[len];
         for (int i = 0; i < len; i++) {
             String tmp = words[i];
             value[i] = 0;
@@ -15,11 +15,12 @@ class Solution {
             }
         }
         int maxProduct = 0;
-        for (int i = 0; i < len; i++)
-            for (int j = i + 1; j < len; j++) {
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j < len; j++) {
                 if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
                     maxProduct = words[i].length() * words[j].length();
             }
+        }
         return maxProduct;
     }
 }

d0d31dcf536ce57bef13da7d257e88aa4bbf9c64
Yonezcy <364251097@qq.com>
2018-10-05 10:04:03 +0800

    10月5日复习算法，将一个整数分解为若干整数的和，求最大乘积

--- Integer Break.java
+++ Integer Break.java
@@ -1,8 +1,8 @@
 // leetcode 343 problem
 // 将一个整数分解为若干整数的和，求最大乘积
 
-// Math Solution
-// 可以证明：3是最理想的乘积因子 https://leetcode.com/problems/integer-break/discuss 第二个解法
+// Math Solution.
+// 可以证明：3是最理想的乘积因子 https://leetcode.com/problems/integer-break/discuss 第二个解法.
 class Solution {
     public int integerBreak(int n) {
         if (n == 2) return 1;
@@ -17,13 +17,13 @@ class Solution {
     }
 }
 
-// DP Solution
+// DP Solution.
 public int integerBreak(int n) {
-    int[] dp = new int[n + 1];
+    int[] dp = new int[n+1];
     dp[1] = 1;
     for (int i = 2; i <= n; i++) {
         for (int j = 1; j < i; j++) {
-           dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
+           dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i-j, dp[i-j]));
        }
    }
    return dp[n];

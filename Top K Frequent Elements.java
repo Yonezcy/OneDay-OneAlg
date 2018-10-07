6557f415d8bdbdabf1ee12174f07f376abdee982
Yonezcy <364251097@qq.com>
2018-10-07 10:31:21 +0800

    10月7日复习算法，寻找数组中出现次数最多的前k个元素

--- Top K Frequent Elements.java
+++ Top K Frequent Elements.java
@@ -2,9 +2,9 @@
 // 寻找数组中出现次数最多的前k个元素
 
 class Solution {
-    public List<Integer> topKFrequent(int[] nums, int k) {
+    public List<Integer> topKFrequent(int nums[], int k) {
 
-        List<Integer>[] bucket = new List[nums.length + 1];
+        List<Integer> bucket[] = new List[nums.length+1];
         Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
         for (int n : nums) {
             frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
@@ -16,9 +16,8 @@ class Solution {
             }
             bucket[frequency].add(key);
         }
-        
         List<Integer> res = new ArrayList<>();
-        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+        for (int pos = bucket.length-1; pos >= 0 && res.size() < k; pos--) {
             if (bucket[pos] != null) {
                 res.addAll(bucket[pos]);
             }

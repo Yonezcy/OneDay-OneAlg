5f9d9ad8c49c4ee9f4dfea58b89a9ae70de2caae
Yonezcy <364251097@qq.com>
2018-10-08 09:56:56 +0800

    10月8日复习算法，找出两个数组的交集

--- Intersection of Two Arrays.java
+++ Intersection of Two Arrays.java
@@ -1,6 +1,7 @@
 // leetcode 349 problem
-// 找出两个数组的公共元素
+// 找出两个数组的交集
 
+// O(n) Solution, using two extra sets.
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>();
@@ -17,6 +18,32 @@ class Solution {
         int i = 0;
         for (Integer num : intersect) {
             result[i++] = num;
+        }
+        return result;
+    }
+}
+
+// O(nlogn) Solution, using one extra set.
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) i++;
+            else if (nums1[i] > nums2[j]) j++;
+            else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
         }
         return result;
     }
---
+++ Merge Sort.c
@@ -0,0 +1,59 @@
+// Merge Sort
+
+// Bottom-up -- Iterative Solution.
+void merge_sort(int arr[], int len) {
+	int* a = arr;
+	int* b = (int*) malloc(len * sizeof(int));
+	int seg, start;
+	for (seg = 1; seg < len; seg += seg) {
+		for (start = 0; start < len; start += seg + seg) {
+			int low = start, mid = min(start + seg, len), high = min(start + seg + seg, len);
+			int k = low;
+			int start1 = low, end1 = mid;
+			int start2 = mid, end2 = high;
+			while (start1 < end1 && start2 < end2)
+				b[k++] = a[start1] < a[start2] ? a[start1++] : a[start2++];
+			while (start1 < end1)
+				b[k++] = a[start1++];
+			while (start2 < end2)
+				b[k++] = a[start2++];
+		}
+		int* temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a != arr) {
+		int i;
+		for (i = 0; i < len; i++)
+			b[i] = a[i];
+		b = a;
+	}
+	free(b);
+}
+int min(int x, int y) {
+	return x < y ? x : y;
+}
+
+// Top-down -- Recursive Solution.
+void merge_sort_recursive(int arr[], int reg[], int start, int end) {
+	if (start >= end)
+		return;
+	int len = end - start, mid = (len >> 1) + start;
+	int start1 = start, end1 = mid;
+	int start2 = mid + 1, end2 = end;
+	merge_sort_recursive(arr, reg, start1, end1);
+	merge_sort_recursive(arr, reg, start2, end2);
+	int k = start;
+	while (start1 <= end1 && start2 <= end2)
+		reg[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+	while (start1 <= end1)
+		reg[k++] = arr[start1++];
+	while (start2 <= end2)
+		reg[k++] = arr[start2++];
+	for (k = start; k <= end; k++)
+		arr[k] = reg[k];
+}
+void merge_sort(int arr[], const int len) {
+	int reg[len];
+	merge_sort_recursive(arr, reg, 0, len - 1);
+}

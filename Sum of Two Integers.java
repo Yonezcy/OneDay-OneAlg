07161437c54a4db077608811561db253825e587a
Yonezcy <364251097@qq.com>
2018-10-12 10:07:47 +0800

    10月12日复习算法，不用+,-实现两整型数相加

--- Sum of Two Integers.java
+++ Sum of Two Integers.java
@@ -1,18 +1,16 @@
 // leetcode 371 problem
-// 不用+-实现两整型数相加
+// 不用+,-实现两整型数相加
 
 // Iterative Solution
 class Solution {
     public int getSum(int a, int b) {
         if (a == 0) return b;
         if (b == 0) return a;
-
         while (b != 0) {
             int carry = a & b;
             a = a ^ b;
             b = carry << 1;
         }
-
         return a;
     }
 }

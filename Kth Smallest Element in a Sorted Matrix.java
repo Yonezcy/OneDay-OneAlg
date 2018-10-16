3a73e23ed4546bd0ffacd347f885a369e40361d2
Yonezcy <364251097@qq.com>
2018-10-16 16:01:25 +0800

    10月16日复习算法，给定一个每一行每一列都是升序的矩阵，找出第k小的元素

--- Kth Smallest Element in a Sorted Matrix.java
+++ Kth Smallest Element in a Sorted Matrix.java
@@ -1,10 +1,10 @@
 // leetcode 378 problem
 // 给定一个每一行每一列都是升序的矩阵，找出第k小的元素
 
-// Binary Search Solution
+// Binary Search Solution.
 public class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1; //[lo, hi)
+        int lo = matrix[0][0], hi = matrix[matrix.length-1][matrix[0].length-1] + 1; //[lo, hi)
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             int count = 0, j = matrix[0].length - 1;

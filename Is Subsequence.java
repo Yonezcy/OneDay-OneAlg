040aa37c108c6aca90c47f94c346bb9c7b3389be
Yonezcy <364251097@qq.com>
2018-10-18 09:45:11 +0800

    10月18日复习算法，判断一个字符串是否为另一个字符串的子串

--- Is Subsequence.java
+++ Is Subsequence.java
@@ -1,9 +1,11 @@
-// leetcode 392th problem
+// leetcode 392 problem
 // 判断一个字符串是否为另一个字符串的子串
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
         if (s.length() == 0) return true;
         int indexS = 0, indexT = 0;
+        // 无需考虑顺序问题
         while (indexT < t.length()) {
             if (t.charAt(indexT) == s.charAt(indexS)) {
                 indexS++;

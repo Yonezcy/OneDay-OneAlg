efdcc84660c746e2f36414d2580f7d851b1febd7
Yonezcy <364251097@qq.com>
2018-10-19 09:38:50 +0800

    10月19日复习算法，找出字符串中每个字符都至少重复k次的最长子串

--- Longest Substring with At Least K Repeating Characters.py
+++ Longest Substring with At Least K Repeating Characters.py
@@ -1,17 +1,10 @@
 # leetcode 395 problem
-# 找出字符串中每个字符都至少重复k次的子串
+# 找出字符串中每个字符都至少重复k次的最长子串
 
+# Brief Recursive Solution.
 class Solution(object):
     def longestSubstring(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        if len(s) < k:
-            return 0
-        c = min(set(s), key=s.count)
-        if s.count(c) >= k:
-            return len(s)
-        return max(self.longestSubstring(t, k) for t in s.split(c))
-        
+        for c in set(s):
+            if s.count(c) < k:
+                return max(self.longestSubstring(t, k) for t in s.split(c))
+        return len(s)

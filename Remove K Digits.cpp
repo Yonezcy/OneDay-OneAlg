3ead728ee9676d77da2a56a75e50672c4e84b0fb
Yonezcy <364251097@qq.com>
2018-10-20 10:23:11 +0800

    10月20日复习算法，在字符串中移除k个字符，使得余下的字符组成的数最小

--- Remove K Digits.cpp
+++ Remove K Digits.cpp
@@ -1,6 +1,7 @@
 // leetcode 402 problem
-// 移除k个数组字符，使得余下的字符组成的数最小
+// 在字符串中移除k个字符，使得余下的字符组成的数最小
 
+// Straight-forward O(k*n) Solution.
 class Solution {
 public:
     string removeKdigits(string num, int k) {
@@ -17,5 +18,28 @@ public:
         while (s < (int)num.size()-1 && num[s] == '0') s++;
         num.erase(0, s);
         return num == "" ? "0" : num;
+    }
+};
+
+// O(n) Solution(using stack).
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string res;
+        int keep = num.size() - k;
+        for (int i = 0; i < num.size(); i++) {
+            while (res.size() > 0 && res.back() > num[i] && k > 0) {
+                res.pop_back();
+                k--;
+            }
+            res.push_back(num[i]);
+        }
+        // prevent only 1 char case
+        res.erase(keep, string::npos);
+        // trim leading zeros
+        int s = 0;
+        while (s < (int)res.size()-1 && res[s] == '0')  s++;
+        res.erase(0, s);
+        return res == "" ? "0" : res;
     }
 };

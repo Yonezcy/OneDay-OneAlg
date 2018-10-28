f495133463b595fd251935454465e308008a0cbc
Yonezcy <364251097@qq.com>
2018-10-28 15:40:24 +0800

    10月28日复习算法，找出数组中所有重复的元素(O(n) with out extra space)

--- Find All Duplicates in an Array.java
+++ Find All Duplicates in an Array.java
@@ -1,5 +1,5 @@
 // leetcode 442 problem
-// 找出数组中所有重复的元素
+// 找出数组中所有重复的元素(O(n) with out extra space)
 
 class Solution {
     // when find a number i, flip the number at position i-1 to negative. 
@@ -7,9 +7,8 @@ class Solution {
     
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; ++i) {
-            // 重复的元素index一样
-            int index = Math.abs(nums[i])-1;
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
             if (nums[index] < 0)
                 res.add(Math.abs(index+1));
             nums[index] = -nums[index];

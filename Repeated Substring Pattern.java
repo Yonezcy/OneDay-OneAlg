68fe6fef2fe52743e490781facabe325737d78b3
Yonezcy <364251097@qq.com>
2018-10-31 09:52:58 +0800

    10月31日复习算法，判断一个字符串是否是由某个子串重复得到

--- Repeated Substring Pattern.java
+++ Repeated Substring Pattern.java
@@ -1,11 +1,6 @@
 // leetcode 459 problem
 // 判断一个字符串是否是由某个子串重复得到
 
-// The length of the repeating substring must be a divisor of the length of the input string
-// Search for all possible divisor of str.length, starting for length/2
-// If i is a divisor of length, repeat the substring from 0 to i the number of times i is contained in s.length
-// If the repeated substring is equals to the input str return true
-
 class Solution {
     public boolean repeatedSubstringPattern(String str) {
         int l = str.length();
@@ -14,10 +9,10 @@ class Solution {
                 int m = l / i;
                 String subS = str.substring(0,i);
                 StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < m; j++) {
+                for (int j = 0; j < m; j++)
                     sb.append(subS);
-                }
-                if (sb.toString().equals(str)) return true;
+                if (sb.toString().equals(str)) 
+                    return true;
             }
         }
         return false;

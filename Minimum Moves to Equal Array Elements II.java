05d547dcd70a5e3a60264d15b7184ee95796de8a
Yonezcy <364251097@qq.com>
2018-11-01 10:02:35 +0800

    11月1日复习算法，将数组的各个元素多次加一或减一，使所有元素都相同，求操作的次数

--- Minimum Moves to Equal Array Elements II.java
+++ Minimum Moves to Equal Array Elements II.java
@@ -1,15 +1,14 @@
 // leetcode 462 problem
-// 将数组的各个元素多次加一或减一，使所有元素都相同
+// 将数组的各个元素多次加一或减一，使所有元素都相同，求操作的次数
 
+// 中位数是target，不论odd or even
 class Solution {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
         int i = 0, j = nums.length - 1;
         int count = 0;
         while (i < j) {
-            count += nums[j]-nums[i];
-            i++;
-            j--;
+            count += nums[j--] - nums[i++];
         }
         return count;
     }

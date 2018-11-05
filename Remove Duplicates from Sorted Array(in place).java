867783f1015b215c041f91ac9759d03eec45539e
Yonezcy <364251097@qq.com>
2018-11-05 20:30:26 +0800

    11月5日复习算法，在有序数组中删除重复的元素，返回数组长度

--- Remove Duplicates from Sorted Array(in place).java
+++ Remove Duplicates from Sorted Array(in place).java
@@ -1,12 +1,13 @@
 // leetcode 26 problem
 // 在有序数组中删除重复的元素，返回数组长度
 
+// Extra Space.
 class Solution {
     public static int removeDuplicates(int[] nums) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         int len = nums.length;
         int validLength = 0;
-        for (int i=0; i<len; i++) {
+        for (int i = 0; i < len; i++) {
             if (!map.containsKey(nums[i])) {
                 validLength++;
                 map.put(nums[i], i);
@@ -16,14 +17,14 @@ class Solution {
     }
 }
 
+// In-place.
 class Solution {
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
         int i = 0;
         for (int j = 1; j < nums.length; j++) {
             if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+                nums[++i] = nums[j];
             }
         }
         return i + 1;

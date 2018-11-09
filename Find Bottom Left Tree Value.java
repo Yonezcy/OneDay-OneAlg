abfbf395b47e89deb5c1fdbb9c666016af2b1aaa
Yonezcy <364251097@qq.com>
2018-11-09 09:53:24 +0800

    11月9日复习算法，求二叉树最底层最左边的结点值

--- Find Bottom Left Tree Value.java
+++ Find Bottom Left Tree Value.java
@@ -1,17 +1,7 @@
 // leetcode 513 problem
 // 求二叉树最底层最左边的结点值
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
-// dfs solution
+// DFS Solution.
 class Solution {
     public int maxDepth = 0;
     public int val = 0;
@@ -27,8 +17,8 @@ class Solution {
                 val = root.val;
                 maxDepth = depth;
             }
-            dfs(root.left, depth + 1);
-            dfs(root.right, depth + 1);
+            dfs(root.left, depth+1);
+            dfs(root.right, depth+1);
         }
     }
 }

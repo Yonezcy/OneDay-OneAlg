3154646b4aae8d467c9ed8d03e3c150b4c1ccb7e
Yonezcy <364251097@qq.com>
2018-11-13 09:47:50 +0800

    11月13日复习算法，找出binary数组0和1的个数相等的最大子串长度

--- Contiguous Array.java
+++ Contiguous Array.java
@@ -1,9 +1,10 @@
 // leetcode 525 problem
-// 找出数组的子串中0和1的个数相等的最大子串长度
+// 找出binary数组0和1的个数相等的最大子串长度
 
 class Solution {
     public int findMaxLength(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
+            // turn 0 to -1
             if (nums[i] == 0) nums[i] = -1;
         }
         
@@ -11,7 +12,7 @@ class Solution {
         sumToIndex.put(0, -1);
         int sum = 0, max = 0;
         
-        // if sum[i, j] == 0  there is equal 0 and 1 between i and j
+        // if sum(i->j) == 0  there is equal -1 and 1 between i and j
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             if (sumToIndex.containsKey(sum)) {

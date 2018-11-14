75acce152bed10d6afb213a8e88fa460c0f3c4dc
Yonezcy <364251097@qq.com>
2018-11-14 09:25:37 +0800

    11月14日复习算法，求出二叉搜索树中结点值的最小绝对值差

--- Minimum Absolute Difference in BST.java
+++ Minimum Absolute Difference in BST.java
@@ -1,14 +1,15 @@
 // leetcode 530 problem
-// 求出二叉搜索树中的结点值的最小绝对值差
+// 求出二叉搜索树中结点值的最小绝对值差
 
 class Solution {
     int min = Integer.MAX_VALUE;
+    // prev is the value of last node in traversal, so it must be less than root because of BST.
     Integer prev = null;
     public int getMinimumDifference(TreeNode root) {
         if (root == null) return min;
         getMinimumDifference(root.left);
         if (prev != null) {
-            min = Math.min(min, root.val - prev);
+            min = Math.min(min, root.val-prev);
         }
         prev = root.val;
         getMinimumDifference(root.right);

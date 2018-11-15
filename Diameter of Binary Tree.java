607e2b941336efc9f80cec83cc1597d6739b2719
Yonezcy <364251097@qq.com>
2018-11-15 09:47:58 +0800

    11月15日复习算法，求二叉树的直径（两个结点间的最长路径）

--- Diameter of Binary Tree.java
+++ Diameter of Binary Tree.java
@@ -1,6 +1,7 @@
 // leetcode 543 problem
 // 求二叉树的直径（两个结点间的最长路径）
 
+// 过一个结点的最长路径一定是左子树与右子树的最大深度之和，遍历所有结点即可找出直径
 class Solution {
     int max = 0;
     public int diameterOfBinaryTree(TreeNode root) {

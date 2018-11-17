d9dcc35ac2dada93137e8b1e01b3e0bc701d82e0
Yonezcy <364251097@qq.com>
2018-11-17 11:29:45 +0800

    11月17日复习算法，Subarray Sum Equals K

--- Subarray Sum Equals K.java
+++ Subarray Sum Equals K.java
@@ -1,5 +1,5 @@
 // leetcode 560 problem
-// 在数组中找出和为k的子串的个数
+// 求和为k的子串的个数
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
@@ -10,9 +10,9 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             
-            // 如果之前有存在presum且presum+k=sum，说明遍历至此一定存在和为k的子序列，数量取决于presum的数量
-            if (preSum.containsKey(sum - k)) {
-                result += preSum.get(sum - k);
+            // 如果之前有存在presum且presum+k=sum，说明遍历至此一定存在和为k的子串，数量取决于presum的数量
+            if (preSum.containsKey(sum-k)) {
+                result += preSum.get(sum-k);
             }
             preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
         }

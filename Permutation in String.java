630adcc338002724cc01181d6b6b01b40a9a34cf
Yonezcy <364251097@qq.com>
2018-11-19 10:21:16 +0800

    11月19日复习算法，判断第二个字符串的子串是否包含第一个字符串的任意排列

--- Permutation in String.java
+++ Permutation in String.java
@@ -1,6 +1,7 @@
 // leetcode 567 problem
 // 判断第二个字符串的子串是否包含第一个字符串的任意排列
 
+// Sliding window + map solution.
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int len1 = s1.length(), len2 = s2.length();
@@ -8,12 +9,16 @@ class Solution {
         
         int[] count = new int[26];
         for (int i = 0; i < len1; i++) {
+            // initialize sliding window between 0, 2
             count[s1.charAt(i) - 'a']++;
             count[s2.charAt(i) - 'a']--;
         }
         if (allZero(count)) return true;
         
         for (int i = len1; i < len2; i++) {
+            // sliding window. When a character moves in from right of the window, 
+            // we subtract 1 to that character count from the map. 
+            // When a character moves out from left of the window, we add 1 to that character count. 
             count[s2.charAt(i) - 'a']--;
             count[s2.charAt(i - len1) - 'a']++;
             if (allZero(count)) return true;

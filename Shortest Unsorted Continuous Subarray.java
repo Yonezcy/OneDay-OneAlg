9b3b652ccb89c62a5cc6f8d81ecf45672c6d2160
Yonezcy <364251097@qq.com>
2018-11-21 10:07:07 +0800

    11月21日复习算法，找出一个数组的最短子串使得子串排序后整个数组呈升序

--- Shortest Unsorted Continuous Subarray.java
+++ Shortest Unsorted Continuous Subarray.java
@@ -4,11 +4,11 @@
 class Solution {
     public int findUnsortedSubarray(int[] A) {
         int n = A.length, begin = -1, end = -2, min = A[n-1], max = A[0];
-        for (int i=1;i<n;i++) {
+        for (int i = 1; i < n; i++) {
           max = Math.max(max, A[i]);
           min = Math.min(min, A[n-1-i]);
           if (A[i] < max) end = i;
-          if (A[n-1-i] > min) begin = n-1-i; 
+          if (A[n-1-i] > min) begin = n - 1 - i;
         }
         return end - begin + 1;
     }

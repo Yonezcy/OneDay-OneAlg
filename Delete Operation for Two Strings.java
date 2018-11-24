9e4ae116b998dc0a0217681162a4ae5e6a6f4cdb
Yonezcy <364251097@qq.com>
2018-11-24 11:33:56 +0800

    11月24日复习算法，找出需要的最小步数使得两个字符串减少至相同（每次只能减一个数）

--- Delete Operation for Two Strings.java
+++ Delete Operation for Two Strings.java
@@ -2,17 +2,16 @@
 // 找出需要的最小步数使得两个字符串减少至相同（每次只能减一个数）
 
 // 思想为先找出最长公共子序列的长度，将两个字符串的长度分别减去最长公共子序列的长度再相加即可
-
 class Solution {
     public int minDistance(String word1, String word2) {
         int dp[][] = new int[word1.length()+1][word2.length()+1];
-        for(int i = 0; i <= word1.length(); i++) {
-            for(int j = 0; j <= word2.length(); j++) {
-                if(i == 0 || j == 0) dp[i][j] = 0;
-                else dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
-                        : Math.max(dp[i-1][j], dp[i][j-1]);
+        for (int i = 0; i <= word1.length(); i++) {
+            for (int j = 0; j <= word2.length(); j++) {
+                if (i == 0 || j == 0) dp[i][j] = 0;
+                else dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1 : Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
+        // 最长公共子序列长度
         int val =  dp[word1.length()][word2.length()];
         return word1.length() - val + word2.length() - val;
     }

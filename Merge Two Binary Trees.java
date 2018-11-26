64c9c4afb40b02662a26c2577be742ac47441e27
Yonezcy <364251097@qq.com>
2018-11-26 09:57:49 +0800

    11月26日复习算法，合并两个二叉树(overlap的节点值等于两颗二叉树的节点的值的和)

--- Merge Two Binary Trees.java
+++ Merge Two Binary Trees.java
@@ -1,6 +1,7 @@
 // leetcode 617 problem
 // 合并两个二叉树(overlap的节点值等于两颗二叉树的节点的值的和)
 
+// Extra space.
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
         if (t1 == null && t2 == null) return null;
@@ -9,5 +10,18 @@ class Solution {
         newNode.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
         newNode.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
         return newNode;
+    }
+}
+
+// In-place operation.
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+      if (t1 == null) return t2;
+      if (t2 != null) {
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+      }
+      return t1;
     }
 }

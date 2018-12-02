d2defbe55aaa26debda58b5419790a52e1013a8f
Yonezcy <364251097@qq.com>
2018-12-02 09:31:44 +0800

    12月2日复习算法，求数组中乘积小于k的子数组的数量

--- Subarray Product Less Than K.java
+++ Subarray Product Less Than K.java
@@ -3,7 +3,9 @@
 
 '''
 1. The idea is always keep an max-product-window less than K;
-2. Every time shift window by adding a new number on the right(j), if the product is greater than k, then try to reduce numbers on the left(i), until the subarray product fit less than k again, (subarray could be empty);
+2. Every time shift window by adding a new number on the right(j), if the product is greater 
+than k, then try to reduce numbers on the left(i), until the subarray product fit less than k again, 
+(subarray could be empty);
 3. Each step introduces x new subarrays, where x is the size of the current window (j + 1 - i);
 example:
 for window (5, 2), when 6 is introduced, it add 3 new subarray: (5, (2, (6)))

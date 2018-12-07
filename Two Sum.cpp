40442bd511f969931049064f1dc308d1dc80044b
Yonezcy <364251097@qq.com>
2018-12-07 16:18:00 +0800

    12月7日复习算法，找出和为target的两个数的index

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+// leetcode 1 problem
+// 找出和为target的两个数的index
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int i, j;
         vector<int> news(2, 0);
-        for (i=0; i<nums.capacity(); i++){
-            for (j=i+1; j<nums.capacity(); j++){
-                if (nums[i]+nums[j] == target){
+        for (i = 0; i < nums.capacity(); i++) {
+            for (j = i+1; j < nums.capacity(); j++) {
+                if (nums[i] + nums[j] == target) {
                     news[0] = i;
                     news[1] = j;
                 }

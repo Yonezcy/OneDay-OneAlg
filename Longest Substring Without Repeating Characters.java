96fca092decfb3df65fb6c76cefc50cacf5393f0
Yonezcy <364251097@qq.com>
2018-12-11 10:04:51 +0800

    12月11日复习算法，找出字符串中没有重复字符的最长子串

--- Longest Substring Without Repeating Characters.java
+++ Longest Substring Without Repeating Characters.java
@@ -5,15 +5,15 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int i, j, ans=0;
-        for (i=0; i<n; i++)
-            for (j=i+1; j<=n; j++)
+        for (i = 0; i < n; i++)
+            for (j = i+1; j <= n; j++)
                 if (isAllUnique(s, i, j)) ans = Math.max(ans, j-i);
         return ans;
     }
     public boolean isAllUnique(String s, int start, int end){
         int i;
         Set<Character> set = new HashSet<>();
-        for (i=start; i<end; i++){
+        for (i = start; i < end; i++) {
             char ch = s.charAt(i);
             if (set.contains(ch)) return false;
             set.add(ch);
@@ -22,17 +22,17 @@ class Solution {
     }
 }
 
-class Solution{
-    public int lengthOfLongestSubstring(String s){
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         Set<Character> set = new HashSet<>();
-        int i=0, j=0, ans=0;
-        while (i<n && j<n){
-            if (!set.contains(s.charAt(j))){
+        int i = 0, j = 0, ans = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
                 set.add(s.charAt(j++));
                 ans = Math.max(ans, j-i);
             }
-            else{
+            else {
                 set.remove(s.charAt(i++));
             }
         }
@@ -40,13 +40,13 @@ class Solution{
     }
 }
 
-class Solution{
-    public int lengthOfLongestSubstring(String s){
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int i, j, ans = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (i=0, j=0; j<n; j++){
-            if (map.containsKey(s.charAt(j))){
+        for (i = 0, j = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
                 i = Math.max(map.get(s.charAt(j)), i);
             }
             ans = Math.max(ans, j-i+1);
@@ -62,17 +62,17 @@ class Solution {
         if (s.length() == 0) return 0;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         int max = 0;
-        // i represent the right pointer, j represent the left point
+        // i represents the right pointer, j represents the left point.
         for (int i = 0, j = 0; i < s.length(); i++) {
             if (map.containsKey(s.charAt(i))) {
-                // the reason why not set j = map.get(s.charAt(i))+1 is that if some characters repeated,
-                // the left pointer should point the last position 
+                // The reason why do not set j = map.get(s.charAt(i))+1 is that if some characters repeated,
+                // the left pointer should point the last position.
                 // For example, abcab, when we scan through it in the end, the left pointer point the second
-                // a, not the first b
+                // a, not the first b.
                 j = Math.max(j, map.get(s.charAt(i))+1);
             }
             map.put(s.charAt(i), i);
-            // Calculate length
+            // Calculate length.
             max = Math.max(max, i-j+1);
         }
         return max;

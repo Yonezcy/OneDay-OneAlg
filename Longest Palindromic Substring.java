52de36ad610d85d24b8311d58cbd1433ddd886a7
Yonezcy <364251097@qq.com>
2018-12-12 10:02:30 +0800

    12月12日复习算法，求字符串的最大回文子串

--- Longest Palindromic Substring.java
+++ Longest Palindromic Substring.java
@@ -25,3 +25,31 @@ class Solution {
         return R - L - 1;
     }
 }
+
+// Similar solution, no start and end pointer.
+public class Solution {
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i+1);
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+}

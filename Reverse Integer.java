af0608f2817c27f71be57777622b0f13eb75a234
Yonezcy <364251097@qq.com>
2018-12-13 09:49:14 +0800

    12月13日复习算法，将一个整形数翻转(考虑溢出)

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -2,17 +2,14 @@
 // 将一个整形数翻转(考虑溢出)
 
 class Solution {
-    public int reverse(int x)
-    {
+    public int reverse(int x) {
         int result = 0;
-        while (x != 0)
-        {
+        while (x != 0) {
             int tail = x % 10;
             int newResult = result * 10 + tail;
-            
             // 溢出前后结果就会不相等
-            if ((newResult - tail) / 10 != result) return 0;
-
+            if ((newResult - tail) / 10 != result) 
+                return 0;
             result = newResult;
             x = x / 10;
         }

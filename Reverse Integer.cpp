71dab741884e18cf0a65107d33ee8e05f36f5f70
Yonezcy <364251097@qq.com>
2018-12-13 09:52:05 +0800

    12月13日复习算法，将一个整形数翻转(考虑溢出)

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,32 +1,35 @@
-#include<iostream>
-using namespace std;
+// leetcode 7 problem
+// 将一个整形数翻转(考虑溢出)
 
 class Solution {
-    public:
-        static int reverse(int x) {
-            long long res = 0;
-            bool isPositive = true;
-            if (x < 0){
-                isPositive = false;
-                x *= -1;
-            }
+public:
+    static int reverse(int x) {
+        long long res = 0;
+        bool isPositive = true;
+        if (x < 0) {
+            isPositive = false;
+            x *= -1;
+        }
+        while (x > 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        if (res > INT_MAX) return 0;
+        if (isPositive) return res;
+        else return -res;
+    }
+};
 
-            while (x > 0){
-                res = res * 10 + x % 10;
-                x /= 10;
-            }
-            if (res > INT_MAX) return 0;
-            if (isPositive) return res;
-            else return -res;
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            // why do not need to check "="?
+            if (abs(res) > INT_MAX / 10) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
         }
-        int reverse2(int x) {
-            int res = 0;
-            while (x != 0) {
-                // why not need to check "=" ?
-                if (abs(res) > INT_MAX / 10) return 0;
-                res = res * 10 + x % 10;
-                x /= 10;
-            }
-            return res;
-        }
+        return res;
+    }
 };

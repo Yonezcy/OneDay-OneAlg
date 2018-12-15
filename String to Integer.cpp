01a6daf9e9491e79f298526950dc8ad63c9febb4
Yonezcy <364251097@qq.com>
2018-12-15 19:19:55 +0800

    12月15日复习算法，把string转换成int(atoi)

--- String to Integer.cpp
+++ String to Integer.cpp
@@ -1,15 +1,17 @@
+// Leetcode 8 problem
+// 把string转换成int(atoi)
+
 class Solution {
 public:
-    int myAtoi(string str)
-    {
+    int myAtoi(string str) {
         if (str == "") return 0;
         int i;
         int sign_count = 0;
         long long res = 0;
         int sign = 1;
         int hasDigit = 0;
-        for (i=0; i<str.length(); i++){
-            if (sign_count == 0 && hasDigit == 0){
+        for (i = 0; i < str.length(); i++) {
+            if (sign_count == 0 && hasDigit == 0) {
                 if (str[i] == ' ') continue;
             }
             if (str[i] == '+' || str[i] == '-') {
@@ -17,12 +19,12 @@ public:
                 sign_count++;
                 continue;
             }
-            if (sign_count <= 1){
-                if (str[i] >= '0' && str[i] <= '9'){
+            if (sign_count <= 1) {
+                if (str[i] >= '0' && str[i] <= '9') {
                     hasDigit = 1;
                     res = res * 10 + (str[i] - '0');
-                    if(res > INT_MAX)
-                        return sign<0 ? INT_MIN : INT_MAX;
+                    if (res > INT_MAX)
+                        return sign < 0 ? INT_MIN : INT_MAX;
                 }
                 else break;
             }

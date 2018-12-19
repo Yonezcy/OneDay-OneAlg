e28181519a622489618ec2b0868a86aab46c3f42
Yonezcy <364251097@qq.com>
2018-12-19 20:37:15 +0800

    12月19日复习算法， 找出字符数组中字符串的最长公共前缀

--- Longest Common Prefix.java
+++ Longest Common Prefix.java
@@ -1,15 +1,15 @@
 // leetcode 14 problem
 // 找出字符数组中字符串的最长公共前缀
 
+// Brute force solution.
 class Solution {
-    // brute force
     public String longestCommonPrefix(String[] strs) {
         int i;
         int maxLength = 0;
         if (strs.length < 2) return " ";
         
         String s3 = strs[1].substring(0, findLengthOfTwoString(strs[0], strs[1]));
-        for (i = 2; i < strs.length; i++){
+        for (i = 2; i < strs.length; i++) {
             maxLength = Math.max(maxLength, findLengthOfTwoString(s3, strs[i]));
             s3 = strs[i].substring(0, maxLength);
         }
@@ -29,23 +29,24 @@ class Solution {
     }
 }
 
+// Horizontal scanning.
 class Solution {
-    // horizontal scanning
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
         String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            // function indexOf() return the first index of prefix in strs[i]
+        for (int i = 1; i < strs.length; i++) {
+            // Function indexOf() return the first index of prefix in strs[i].
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length()-1);
                 if (prefix.isEmpty()) return "";
             }
+        }
         return prefix;
     }
 }
 
+// Vertical scanning.
 class Solution {
-    // vertical scanning
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
         for (int i = 0; i < strs[0].length(); i++) {

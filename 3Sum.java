c86f599ad4bed5d35ccba045245c8683854d240d
Yonezcy <364251097@qq.com>
2018-12-21 12:00:20 +0800

    12月21日复习算法，3 sum problem

--- 3Sum.java
+++ 3Sum.java
@@ -1,21 +1,22 @@
+// leetcode 15 problem
+// 3 sum problem
+
+// Brute force solution.
 class Solution {
-    // brute force
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> bigList = new ArrayList<List<Integer>>();
-        
         int i, j, k, m;
-        
-        for (i=0; i<nums.length-2; i++)
-            for (j=i+1; j<nums.length-1; j++)
-                for (k=j+1; k<nums.length; k++) {
+        for (i = 0; i < nums.length-2; i++)
+            for (j = i+1; j < nums.length-1; j++)
+                for (k = j+1; k < nums.length; k++) {
                     int sign = 0;
-                    if ((nums[i] + nums[j] + nums[k]) == 0) {
+                    if ((nums[i]+nums[j]+nums[k]) == 0) {
                         List<Integer> smallList = new ArrayList<>();
                         smallList.add(nums[i]);
                         smallList.add(nums[j]);
                         smallList.add(nums[k]);
-                        for (m=0; m<bigList.size(); m++) {
-                            if (bigList.get(m).containsAll(smallList) && smallList.containsAll(bigList.get(m))){
+                        for (m = 0; m < bigList.size(); m++) {
+                            if (bigList.get(m).containsAll(smallList) && smallList.containsAll(bigList.get(m))) {
                                 sign = 1;
                             }
                         }

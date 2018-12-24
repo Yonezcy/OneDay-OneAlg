51ce3dfc79ad33aca8dd3456cd87074197d320c0
Yonezcy <364251097@qq.com>
2018-12-24 18:49:27 +0800

    122月24日复习算法，求出指定的手机按键的字母组合

--- Letter Combinations of a Phone Number.cpp
+++ Letter Combinations of a Phone Number.cpp
@@ -8,12 +8,11 @@ public:
         if (digits.size() == 0) return res;
         string charmap[10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         res.push_back("");
-        for (int i = 0; i < digits.size(); i++)
-        {
+        for (int i = 0; i < digits.size(); i++) {
             vector<string> tempres;
-            string chars = charmap[digits[i] - '0'];
-            for (int c = 0; c < chars.size();c++)
-                for (int j = 0; j < res.size();j++)
+            string chars = charmap[digits[i]-'0'];
+            for (int c = 0; c < chars.size(); c++)
+                for (int j = 0; j < res.size(); j++)
                     tempres.push_back(res[j]+chars[c]);
             res = tempres;
         }

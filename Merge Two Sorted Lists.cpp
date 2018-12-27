6eb7794b30b73e349193eaba4be062edcdbcd563
Yonezcy <364251097@qq.com>
2018-12-27 11:32:26 +0800

    12月27日复习算法，按升序合并两个链表

--- Merge Two Sorted Lists.cpp
+++ Merge Two Sorted Lists.cpp
@@ -1,31 +1,32 @@
 // leetcode 21 problem
 // 按升序合并两个链表
 
+// General soluton.
 class Solution {  
 public:  
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {  
         if(l1 == NULL) return l2;
         if(l2 == NULL) return l1;
         
-        ListNode head(-1);  
-        ListNode *tmp = &head;  
+        ListNode head(-1);
+        ListNode *tmp = &head;
         while (l1 && l2) {  
             if (l1->val < l2->val) {  
-                tmp->next = l1;  
-                l1 = l1->next;  
+                tmp->next = l1;
+                l1 = l1->next; 
             } else {  
-                tmp->next = l2;  
-                l2 = l2->next;  
+                tmp->next = l2;
+                l2 = l2->next; 
             }  
-            tmp = tmp->next;  
+            tmp = tmp->next;
         }  
-        if (l1) tmp->next = l1;  
-        if (l2) tmp->next = l2;  
-        return head.next;  
+        if (l1) tmp->next = l1;
+        if (l2) tmp->next = l2;
+        return head.next;
     }  
 }; 
 
-// Recursive Solution
+// Recursive Solution.
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {

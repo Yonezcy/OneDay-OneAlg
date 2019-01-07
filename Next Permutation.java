7ed2843db1a301e7ca0eb2294bd85f53166ef5ab
Yonezcy <364251097@qq.com>
2019-01-07 11:53:17 +0800

    1月7日复习算法，找出一维数组的下一个排列

--- Next Permutation.java
+++ Next Permutation.java
@@ -4,21 +4,18 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        while (i >= 0 && nums[i+1] <= nums[i]) {
-            i--;
-        }
+        while (i >= 0 && nums[i+1] <= nums[i]) i--;
         if (i >= 0) {
             int j = nums.length - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
-            }
+            while (j >= 0 && nums[j] <= nums[i]) j--;
             swap(nums, i, j);
         }
         reverse(nums, i+1);
     }
 
     private void reverse(int[] nums, int start) {
-        int i = start, j = nums.length - 1;
+        int i = start;
+        int j = nums.length - 1;
         while (i < j) {
             swap(nums, i, j);
             i++;

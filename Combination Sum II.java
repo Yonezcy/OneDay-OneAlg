854213432e30167e8894242db150211ce4189336
Yonezcy <364251097@qq.com>
2019-01-18 15:57:17 +0800

    1月18日复习算法，求一维数组的所有子集

--- Combination Sum II.java
+++ Combination Sum II.java
@@ -11,7 +11,7 @@ class Solution {
 
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
         if (remain < 0) return;
         else if (remain == 0) list.add(new ArrayList<>(tempList));
         else {
@@ -19,7 +19,7 @@ class Solution {
                 if (i > start && nums[i] == nums[i-1]) continue; // skip duplicates
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums, remain-nums[i], i+1);
-                tempList.remove(tempList.size()-1); 
+                tempList.remove(tempList.size()-1);
             }
         }
     } 

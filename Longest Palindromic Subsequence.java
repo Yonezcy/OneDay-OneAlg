aa159181d06619b4207315e164e4c01c2bbbf23c
Yonezcy <364251097@qq.com>
2019-01-24 11:23:52 +0800

    1月24日复习算法，求最大回文子序列的长度

--- Longest Palindromic Subsequence.java
+++ Longest Palindromic Subsequence.java
@@ -3,16 +3,16 @@
 
 // dp[i][j]: the longest palindromic subsequence’s length of substring(i, j)
 // State transition:
-// dp[i][j] = dp[i+1][j-1] + 2 if s.charAt(i) == s.charAt(j)
+// if s.charAt(i) == s.charAt(j) dp[i][j] = dp[i+1][j-1] + 2
 // otherwise, dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1])
 // Initialization: dp[i][i] = 1
 
+
+// i--, j++
 public class Solution {
     public int longestPalindromeSubseq(String s) {
         if (s.length() == 0) return 0; 
         int[][] dp = new int[s.length()][s.length()];
-        
-        // 求dp[0][n-1]有可能要用到dp[1][n-1]，所以i必须倒着循环
         for (int i = s.length()-1; i >= 0; i--) {
             dp[i][i] = 1;
             for (int j = i+1; j < s.length(); j++) {
@@ -26,3 +26,24 @@ public class Solution {
         return dp[0][s.length()-1];
     }
 }
+
+// i++, j--
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        if (s.length() == 0) return 0; 
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+            for (int j = i-1; j >= 0; j--) {
+                if (s.charAt(j) == s.charAt(i)) 
+                    dp[j][i] = dp[j+1][i-1] + 2;
+                else 
+                    dp[j][i] = Math.max(dp[j+1][i], dp[j][i-1]);
+                
+            }
+        }
+        return dp[0][s.length()-1];
+    }
+}
+
+

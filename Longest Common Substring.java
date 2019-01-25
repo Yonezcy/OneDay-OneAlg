682293b61431c7eeca2240d7c2ffc4dbdaf72fdf
Yonezcy <364251097@qq.com>
2019-01-25 15:01:05 +0800

    1月25日增加算法，Longest Common Substring

---
+++ Longest Common Substring.java
@@ -0,0 +1,25 @@
+// Longest Common Substring
+
+class Solution {
+	public int longestCommonSubstr(String s, String t) {
+		if (s.length() == 0 || t.length() == 0) return 0;
+		int[][] dp = new int[s.length()][t.length()];
+		int maxLength = 0;
+		for (int i = 0; i < s.length(); i++) {
+			dp[i][0] = 0;
+		}
+		for (int j = 0; j < t.length(); j++) {
+			dp[0][j] = 0;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = 0; j < t.length(); j++) {
+				if (s.charAt(i-1) == t.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+					maxLength = Math.max(maxLength, dp[i][j]);
+				}
+				else dp[i][j] = 0;
+			}
+		}
+		return maxLength;
+	}
+}

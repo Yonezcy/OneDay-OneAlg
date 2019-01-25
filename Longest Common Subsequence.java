6b63f47341a20a5d55be166c6d77cb709a5e44a1
Yonezcy <364251097@qq.com>
2019-01-25 15:05:10 +0800

    1月25日复习算法，Longest Common Subsequence

--- Longest Common Subsequence.java
+++ Longest Common Subsequence.java
@@ -1,24 +1,22 @@
-// 求两个数组的最长公共子序列的长度(LCS问题)
+// Longest Common Subsequence
 
 class Solution {
-    public int findLength(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
-        if (m == 0 || n == 0) return 0;
-        // dp[i][j] is the length of longest common subsequence ending with A[i-1] and B[j-1].
-        int[][] dp = new int[m+1][n+1];
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
+    public int findLength(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) return 0;
+        // dp[i][j] is the max length of longest common subsequence ending with A[i-1] and B[j-1].
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= t.length(); j++) {
                 if (i == 0 || j == 0)
                     dp[i][j] = 0;
                 else {
-                    if (A[i-1] == B[j-1])
-                        dp[i][j] = 1 + dp[i-1][j-1];
+                    if (s.charAt(i-1) == t.charAt(j-1))
+                        dp[i][j] = dp[i-1][j-1] + 1;
                     else
                         dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        return dp[m][n];
+        return dp[s.length()+1][t.length()+1];
     }
 }

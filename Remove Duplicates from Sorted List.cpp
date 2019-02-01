6d5f6c335a41bb9fedeb3e4e607101ab6fc4b3bf
Yonezcy <364251097@qq.com>
2019-02-01 15:38:18 +0800

    2月1日复习算法，删除链表中的重复元素

--- Remove Duplicates from Sorted List.cpp
+++ Remove Duplicates from Sorted List.cpp
@@ -1,16 +1,15 @@
-// leetcode 82th problem
+// leetcode 83th problem
 // 删除链表中的重复元素
 
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if (head == NULL) return head;
-        ListNode* start = (ListNode*)malloc(sizeof(ListNode));
-        start = head;
+        ListNode* start = head;
         while (start->next) {
-            if (start->next->val != start->val)
-                start = start->next;
-            else start->next = start->next->next; 
+            if (start->next->val == start->val)
+                start->next = start->next->next;
+            else start = start->next; 
         }
         return head;
     }

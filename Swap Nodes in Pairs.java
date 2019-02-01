8c8487f8fdc1a9c6e8cc92aed7bee9f9f31c4fc1
Yonezcy <364251097@qq.com>
2019-02-01 14:49:48 +0800

    2月1日复习算法，两两交换链表中的相邻结点

--- Swap Nodes in Pairs.java
+++ Swap Nodes in Pairs.java
@@ -3,23 +3,19 @@
 
 class Solution {
     public ListNode swapPairs(ListNode head) {
-      if(head == null || head.next == null)
-        return head;
-    
-      ListNode fakehead = new ListNode(-1);
-      fakehead.next = head;
-      
-      ListNode ptr1 = fakehead;
-      ListNode ptr2 = head;
-      
-      while(ptr2!=null && ptr2.next!=null){
-          ListNode nextstart = ptr2.next.next;
-          ptr2.next.next = ptr2;
-          ptr1.next = ptr2.next;
-          ptr2.next = nextstart;
-          ptr1 = ptr2;
-          ptr2 = ptr2.next;
-      }
-    return fakehead.next;
-  }
+        if (head == null || head.next == null) return head;
+        ListNode fakehead = new ListNode(-1);
+        fakehead.next = head;
+        ListNode ptr1 = fakehead;
+        ListNode ptr2 = head;
+        while (ptr2 != null && ptr2.next != null) {
+            ListNode nextstart = ptr2.next.next;
+            ptr2.next.next = ptr2;
+            ptr1.next = ptr2.next;
+            ptr2.next = nextstart;
+            ptr1 = ptr2;
+            ptr2 = ptr2.next;
+        }
+        return fakehead.next;
+    }
 }

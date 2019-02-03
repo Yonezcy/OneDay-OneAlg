5b1f2153d26c878f2ea316ebf4022963091feb6c
Yonezcy <364251097@qq.com>
2019-02-03 11:21:23 +0800

    2月3日增加算法，找出链表中循环开始的结点

--- Linked List Cycle II.cpp
+++ Linked List Cycle II.cpp
@@ -14,6 +14,7 @@ public:
 
             // It can be concluded that the distance between meeting location and cycle begin location is equal
             // to distance between head and cycle begin.
+            // Details refer to https://leetcode.com/problems/linked-list-cycle-ii/discuss/44781/Concise-O(n)-solution-by-using-C%2B%2B-with-Detailed-Alogrithm-Description
             if (slow == fast) {                      
                 while (slow != entry) {              
                     slow  = slow->next;

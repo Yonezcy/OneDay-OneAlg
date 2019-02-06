a9de12e357ff3dc2ce6fe532dcc6fba01506efee
Yonezcy <364251097@qq.com>
2019-02-06 23:00:55 +0800

    2月6日复习算法，将链表中的奇数索引结点移到偶数号索引结点的前面

--- Odd Even Linked List.java
+++ Odd Even Linked List.java
@@ -1,9 +1,9 @@
 // leetcode 328 problem
-// 将链表中的奇数号的结点移到偶数号结点的前面
+// 将链表中的奇数索引结点移到偶数号索引结点的前面
 
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null) return null;
+        if (head == null) return head;
         ListNode odd = head, even = head.next, evenHead = even; 
         while (even != null && even.next != null) {
             odd.next = odd.next.next; 
@@ -11,7 +11,7 @@ class Solution {
             odd = odd.next;
             even = even.next;
         }
-        odd.next = evenHead; 
+        odd.next = evenHead;
         return head;
     }
 }

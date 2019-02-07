7559c8763a9019e2bba1ecfaebe037f5753360ad
Yonezcy <364251097@qq.com>
2019-02-07 11:13:41 +0800

    2月7日复习算法，判断一个链表中是否有环

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -1,6 +1,7 @@
 // leetcode 141 problem
-// 判断一个链表中是否有循环
+// 判断一个链表中是否有环
 
+// Runner and walker solution.
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
@@ -12,6 +13,20 @@ public class Solution {
             walker = walker.next;
             runner = runner.next.next;
             if (walker == runner) return true;
+        }
+        return false;
+    }
+}
+
+// Destroy the bridge after crossed the river. Think the case [1,2,3,4,5,6,3].
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode p = head,pre = head;
+        while (p != null && p.next != null) {
+            if (p.next == head) return true;
+            p = p.next;
+            pre.next = head;
+            pre = p;
         }
         return false;
     }

1dff931e2505c77e9a868599f357e740fc503f7c
Yonezcy <364251097@qq.com>
2019-02-08 13:54:15 +0800

    2月8日复习算法，判断一个链表中是否有环

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -5,7 +5,6 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
-
         // 设置两个指针，runner每次移动两步，walker一步，如果有循环总会有相遇的时候
         ListNode walker = head;
         ListNode runner = head;
@@ -21,7 +20,7 @@ public class Solution {
 // Destroy the bridge after crossed the river. Think the case [1,2,3,4,5,6,3].
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode p = head,pre = head;
+        ListNode p = head, pre = head;
         while (p != null && p.next != null) {
             if (p.next == head) return true;
             p = p.next;

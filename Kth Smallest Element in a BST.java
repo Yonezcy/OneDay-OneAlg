448fb573b0bbae4bce5ef2f35e13675a6ad8b679
Yonezcy <364251097@qq.com>
2019-03-04 19:40:11 +0800

    3月4日复习算法，找出二叉搜索树中第k小的结点的值

--- Kth Smallest Element in a BST.java
+++ Kth Smallest Element in a BST.java
@@ -5,11 +5,8 @@
 class Solution {
      public int kthSmallest(TreeNode root, int k) {
         int count = countNodes(root.left);
-        if (k <= count) {
-            return kthSmallest(root.left, k);
-        } else if (k > count + 1) {
-            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
-        }
+        if (k <= count) return kthSmallest(root.left, k);
+        else if (k > count+1) return kthSmallest(root.right, k-1-count); // 1 is counted as current node
         return root.val;
     }
     

9f860cfa77b35d5a285704d6644d27f8c520bead
Yonezcy <364251097@qq.com>
2019-03-06 22:14:36 +0800

    3月6日复习算法，给定一个总和和若干整数，求它们相加得到总和用到的的最少个数

--- Coin Change.java
+++ Coin Change.java
@@ -6,7 +6,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
         int n = Integer.MAX_VALUE;
-        for (int coin: coins) {
+        for (int coin : coins) {
             int curr = 0;
             if (coin <= amount) {
                 int next = coinChange(coins, amount-coin);
@@ -23,20 +23,20 @@ class Solution {
 
 // 利用map把中间的过程存储起来，用空间换时间，减少时间复杂度
 public class Solution {
-    Map<Integer,Integer> amountDict = new HashMap<Integer,Integer>();
+    Map<Integer, Integer> amountDict = new HashMap<Integer, Integer>();
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
         if (amountDict.containsKey(amount))
             return amountDict.get(amount);
         int n = Integer.MAX_VALUE;
-        for(int coin: coins) {
+        for (int coin : coins) {
             int curr = 0;
             if (amount >= coin) {
                 int next = coinChange(coins, amount-coin);
-                if(next >= 0)
-                    curr = 1 + next;
+                if (next >= 0)
+                    curr = next + 1;
             }
-            if(curr > 0)
+            if (curr > 0)
                 n = Math.min(n,curr);
         }
         int finalCount = (n == Integer.MAX_VALUE) ? -1 : n;

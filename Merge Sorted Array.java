1d65fc5de9194279d8a178a3b6223468fb83f998
Yonezcy <364251097@qq.com>
2019-03-08 17:50:02 +0800

    3月8日复习算法，合并两个有序数组（通过在两个数组分别设置指针实现）

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -6,8 +6,7 @@ class Solution {
         int i = m - 1;
 		int j = n - 1;
 		int k = m + n - 1;
-		while (i >= 0 && j >= 0)
-		{
+		while (i >= 0 && j >= 0) {
 		    if (nums1[i] > nums2[j])
 				nums1[k--] = nums1[i--];
 			else

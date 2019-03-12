a076b8e628210d082b4fabf4206e48854737af3d
Yonezcy <364251097@qq.com>
2019-03-12 20:05:28 +0800

    3月12日复习算法，将单链表进行排序

--- Sort List.java
+++ Sort List.java
@@ -7,17 +7,16 @@ public class Solution {
     if (head == null || head.next == null) return head;
 
     // step 1. cut the list into two halves.
-    ListNode prev = null, slow = head, fast = head;
+    ListNode slow = head, fast = head;
     while (fast != null && fast.next != null) {
-      prev = slow;
       slow = slow.next;
       fast = fast.next.next;
     }
-    prev.next = null;
     
     // step 2. sort each half.
+    ListNode l2 = sortList(slow->next);
+    slow.next = null;
     ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
     
     // step 3. merge l1 and l2.
     return merge(l1, l2);

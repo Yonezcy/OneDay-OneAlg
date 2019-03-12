e7656bac64ffae3e813ff0b3f26e5ae534435456
Yonezcy <364251097@qq.com>
2019-03-12 19:47:24 +0800

    3月12日复习算法，按升序合并两个链表

--- Merge Two Sorted Lists.cpp
+++ Merge Two Sorted Lists.cpp
@@ -5,8 +5,8 @@
 class Solution {  
 public:  
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {  
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
         
         ListNode head(-1);
         ListNode *tmp = &head;
@@ -30,10 +30,10 @@ public:
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
         
-        if(l1->val < l2->val) {
+        if (l1->val < l2->val) {
             l1->next = mergeTwoLists(l1->next, l2);
             return l1;
         } else {

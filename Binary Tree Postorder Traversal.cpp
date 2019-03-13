8ecd1cae8030df188ec6c0c1c4b88feaab7f0fc3
Yonezcy <364251097@qq.com>
2019-03-13 11:18:19 +0800

    3月13日复习算法，二叉树后序遍历序列（迭代解法）

---
+++ Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,25 @@
+// leetcode 145 problem
+// 二叉树后序遍历序列（迭代解法）
+
+// 前序 根 左 右 -> 根 右 左 reverse-> 左 右 根 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+       vector<int> res;
+       if (!root)
+           return res;
+       stack<TreeNode *> st;
+       st.push(root);
+       while (st.size()) {
+           TreeNode *temp = st.top();
+           st.pop();
+           res.push_back(temp->val);
+           if (temp->left)
+               st.push(temp->left);
+           if (temp->right)
+               st.push(temp->right);
+       }
+       reverse(res.begin(),res.end());
+       return res;
+   }
+};

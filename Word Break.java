18c33bdf6743dd384e3807700a4234d4d25fee54
Yonezcy <364251097@qq.com>
2019-03-15 10:11:36 +0800

    3月15日复习算法，判断一个字符串是否能由集合里的字符串组成

---
+++ Word Break.java
@@ -0,0 +1,18 @@
+// leetcode 139 problem
+// 判断一个字符串是否能由集合里的字符串组成
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean dp[] = new boolean[s.length()+1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}

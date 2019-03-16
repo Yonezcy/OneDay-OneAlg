c4fafaaa64cbf7ee7409ed61ee354b4be464e1c1
Yonezcy <364251097@qq.com>
2019-03-16 17:55:27 +0800

    3月16日复习算法，Candy(字节跳动2019笔试第三题)

--- Candy.cpp
+++ Candy.cpp
@@ -1,29 +1,24 @@
 // leetcode 135 problem
-// Candy
+// Candy(字节跳动2019笔试第三题)
 
 class Solution {
 public:
-    int candy(vector<int> &ratings) 
-    {
+    int candy(vector<int> &ratings) {
         int size=ratings.size();
-         if(size<=1)
-             return size;
-         vector<int> num(size,1);
-         for (int i = 1; i < size; i++)
-         {
-             if(ratings[i]>ratings[i-1])
-                 num[i]=num[i-1]+1;
-         }
-         for (int i= size-1; i>0 ; i--)
-         {
-             if(ratings[i-1]>ratings[i])
-                 num[i-1]=max(num[i]+1,num[i-1]);
-         }
-         int result=0;
-         for (int i = 0; i < size; i++)
-         {
-             result+=num[i];
-         }
-         return result;
+        if (size <= 1) return size;
+        vector<int> num(size, 1);
+        for (int i = 1; i < size; i++) {
+            if (ratings[i] > ratings[i-1])
+                num[i] = num[i-1] + 1;
+        }
+        for (int i = size-1; i > 0; i--) {
+            if (ratings[i-1] > ratings[i])
+                num[i-1] = max(num[i]+1, num[i-1]);
+        }
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            result += num[i];
+        }
+        return result;
     }
 };

730623b4841808464ea3cb182cd6746fe50086cb
Yonezcy <364251097@qq.com>
2019-03-17 15:23:24 +0800

    3月17日复习算法，找出字符数组中字符串的最长公共前缀，修改vertical scanning方法返回值

--- Longest Common Prefix.java
+++ Longest Common Prefix.java
@@ -67,13 +67,14 @@ class Solution {
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-            for (int j = i; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != c)
-                    return strs[0].substring(0, i);
+        for (int j = 0; j < strs[0].length(); j++) {
+            char c = strs[0].charAt(j);
+            for (int i = 0; i < strs.length; i++) {
+                if (strs[i].length() == j || strs[i].charAt(j) != c) {
+                    return strs[i].substring(0, j);
+                }
             }
         }
-        return "";
+        return strs[0];
     }
 }

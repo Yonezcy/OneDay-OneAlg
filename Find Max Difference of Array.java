3e21e0d72c58c4dc67f918d9e5038006de370906
Yonezcy <364251097@qq.com>
2019-03-21 15:53:33 +0800

    3月21日复习算法，求数组中两个数相减的最大值

--- Find Max Difference of Array.java
+++ Find Max Difference of Array.java
@@ -1,11 +1,10 @@
 // 求数组中两个数相减的最大值
 // dp解法，每一步循环都找到差值的max和最小元素min
+
 class Solution {
-	public int max_difference(int[] a){
+	public int max_difference(int[] a) {
         int len = a.length;
-        if (len < 2) {
-            return 0;
-        }
+        if (len < 2) return 0;
         int min = Math.min(a[0], a[1]);
         int max_diff = a[1] - a[0];
         for (int i = 2; i < len; i++) {

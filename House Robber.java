643e519484909ba63e7bdc1e7a7b9951a3dc075b
Yonezcy <364251097@qq.com>
2019-04-01 20:54:49 +0800

    4月1日复习算法，求数组中包含不相邻元素的子序列的和最大值

--- House Robber.java
+++ House Robber.java
@@ -1,19 +1,17 @@
 // leetcode 198th problem
-// 求数组中不相邻元素的和的最大值
+// 求数组中包含不相邻元素的子序列的和最大值
 
 // DP Solution
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
-        int dp[] = new int[n]; // Total sum.
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
         }
-        return dp[n-1];
+        return dp[nums.length-1];
     }
 }

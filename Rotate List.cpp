9f73400481a977a38f95308a62b82b8d669826b6
Yonezcy <364251097@qq.com>
2019-05-12 13:30:48 +0800

    5月12日复习算法，旋转链表，将链表后k个结点按顺序移到前面

--- Rotate List.cpp
+++ Rotate List.cpp
@@ -4,29 +4,22 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-
         if (head == NULL || k == 0) return head;
-        ListNode* left = (ListNode*)malloc(sizeof(ListNode));
-        ListNode* newLeft = (ListNode*)malloc(sizeof(ListNode));
-        
-        left = head;
-        newLeft = head;
+        ListNode* left = head;
+        // Length of list.
         int length = 0;
-        
         while (left->next) {
             length++;
             left = left->next;
         }
         length++;
         left->next = head;
-
         k = k % length;
         for (int i = 0; i < length-k; i++) {
             left = left->next;
         }
-        newLeft = left->next;
+        ListNode* newLeft = left->next;
         left->next = NULL;
-        
         return newLeft;
     }        
 };  

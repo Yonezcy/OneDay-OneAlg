e1d7a5f2013ec71c44ba6f7636707d0dd8a1fc96
Yonezcy <364251097@qq.com>
2019-10-05 11:55:31 +0800

    层序遍历二叉树

--- Binary Tree Level Order Traversal.java
+++ Binary Tree Level Order Traversal.java
@@ -1,6 +1,7 @@
 // leetcode 102 problem
 // 层序遍历二叉树
 
+// 输出为list嵌套
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
@@ -14,5 +15,24 @@ class Solution {
         res.get(height).add(root.val);
         levelHelper(res, root.left, height+1);
         levelHelper(res, root.right, height+1);
+    }
+}
+
+// 输出为1个list
+class Solution {
+    public List<Integer> levelOrder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new Queue<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode t = queue.top();
+            queue.pop();
+            res.add(t.val);
+            if (t.left)
+                queue.offer(t.left);
+            if (t.right)
+                queue.offer(t.right);
+        }
+        return res;
     }
 }
